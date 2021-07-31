bbe79bf1aa3b21c8e1b5faab343b1c6c751424b9
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-07-31 19:36:21 +0800

    删除chat-server项目

--- chat-server/src/main/java/com/future/demo/chat/server/common/BaseResponse.java
+++
@@ -1,9 +0,0 @@
-package com.future.demo.chat.server.common;
-
-import lombok.Data;
-
-@Data
-public abstract class BaseResponse {
-    private int errorCode = 0;
-    private String errorMessage = null;
-}
