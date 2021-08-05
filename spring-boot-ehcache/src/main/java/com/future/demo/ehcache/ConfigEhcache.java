799fa0c05f59cb2ad142761c08f197f2c92eb816
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-08-05 17:40:36 +0800

    spring-boot-ehcache新增ConfigEhcache

---
+++ spring-boot-ehcache/src/main/java/com/future/demo/ehcache/ConfigEhcache.java
@@ -0,0 +1,20 @@
+package com.future.demo.ehcache;
+
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class ConfigEhcache {
+    /**
+     * 必需定义EhCacheManagerFactoryBean否则发布版无法注入cacheManager
+     * @return
+     */
+    @Bean
+    public EhCacheManagerFactoryBean cacheManagerFactoryBean(){
+        EhCacheManagerFactoryBean bean = new EhCacheManagerFactoryBean();
+        bean.setConfigLocation(new ClassPathResource("ehcache.xml"));
+        return bean;
+    }
+}
