b4004299bca622a0bad35916c29887cad89d91c8
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-08-08 10:23:16 +0800

    改进spring-boot-quartz demo

---
+++ spring-boot-quartz/README.md
@@ -0,0 +1,2 @@
+##### cron表达式
+http://www.quartz-scheduler.org/documentation/quartz-2.3.0/tutorials/crontrigger.html
--- spring-boot-quartz/src/main/java/com/future/demo/java/spring/boot/quartz/Task1.java
+++ spring-boot-quartz/src/main/java/com/future/demo/java/spring/boot/quartz/Task1.java
@@ -14,12 +14,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class Task1 {
 	/**
-	 * 
+	 *
 	 */
 	@Scheduled(initialDelay = 0, fixedDelay = 60000)
     public void initialTask() {
         Date date = new Date();
         System.out.println("InitialTask " + date);
+    }
+
+    private boolean hasRun = false;
+    @Scheduled(initialDelay = 0, fixedDelay = 1000)
+    public void onlyOnceTask() {
+        // 保证任务只执行一次
+        synchronized (this) {
+            if (hasRun) {
+                return;
+            }
+            hasRun = true;
+        }
+        Date date = new Date();
+        System.out.println("onlyOnceTask " + date);
     }
 
     /**
@@ -29,5 +43,25 @@ public class Task1 {
     public void cronbTask() {
         Date date = new Date();
         System.out.println("cronbTask " + date);
+    }
+
+    // 每分钟执行一次
+    @Scheduled(cron = "0 * * * * ?")
+    public void doCronbEveryMinute() {
+        Date date = new Date();
+        System.out.println("doCronbEveryMinute " + date);
+    }
+
+    // 每秒钟执行一次
+    @Scheduled(cron = "0/1 * * * * ?")
+    public void doCronbEverySecond() {
+        Date date = new Date();
+        System.out.println("doCronbEverySecond " + date);
+    }
+
+    @Scheduled(cron = "0 0/2 10 * * ?")
+    public void doCronbStartAt10Every2Minute() {
+        Date date = new Date();
+        System.out.println("doCronbStartAt10Every2Minute " + date);
     }
 }
