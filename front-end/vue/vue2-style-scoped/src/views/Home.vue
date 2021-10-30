89763a4f7e225a29ec4d2185a558deb3f6071b03
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-10-30 20:08:26 +0800

    新增front-end/vue/vue2-style-scoped demo

---
+++ front-end/vue/vue2-style-scoped/src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <el-image>
+      <div slot="error" class="image-slot">
+        <i class="el-icon-picture-outline"></i>
+      </div>
+    </el-image>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+/* 
+Vue中的scoped的实现原理以及scoped穿透的用法
+https://blog.csdn.net/sinat_17775997/article/details/83416617
+*/
+// 方法1
+.el-image {
+  width: 160px;
+  height: 160px;
+  border-radius: 5px;
+  border-style: solid;
+  border-width: 1px;
+
+  /deep/ .image-slot {
+    font-size: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    color: #a3a3a3;
+  }
+}
+</style>
