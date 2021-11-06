c673e6228d00fcd91f227f4786d14fd033bb33a2
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-11-06 23:32:33 +0800

    新增 front-end/library/element-ui/element-ui-dialog demo

---
+++ front-end/library/element-ui/element-ui-dialog/src/components/el-dialog-demo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
+
+        <!-- NOTE: 下面传递props.visible一定要使用:visible.sync写法，否则子组件不能使用this.$emit('update:visible', false)方法修改父组件data中visible值 -->
+        <my-el-dialog-biz
+            :visible.sync="dialogVisible" 
+            @cancelCallback="handleCancelCallback"
+            @confirmCallback="handleConfirmCallback"></my-el-dialog-biz>    
+    </div>
+</template>
+
+<script>
+import MyElDialogBiz from '@/components/my-el-dialog-biz'
+
+export default {
+    components: {
+        MyElDialogBiz
+    },
+    data() {
+        return {
+            dialogVisible: false,
+        }
+    },
+    methods: {
+      handleCancelCallback(count) {
+          this.$message(`点击取消count=${count}`)
+      },
+      handleConfirmCallback(count) {
+          this.$message(`点击确定count=${count}`)
+      }
+    }
+}
+</script>
