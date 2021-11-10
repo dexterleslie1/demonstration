fb302847665cf17c6cb5de63e82c6896b202d932
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-11-10 17:08:22 +0800

    新增element-ui-image新增el-image-usage.vue demo用例

---
+++ front-end/library/element-ui/element-ui-image/src/components/el-image-usage.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-image 
+        :src="imageUrl" 
+        style="
+            height: 40px;
+            width: 40px;"></el-image>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            imageUrl: 'http://42.240.131.221:9085/obs-default-epu-9223372036854775807-1/2021-09-14/id-6086-qaz1wsx12-Avatar-small.jpg'
+        }
+    }
+}
+</script>
