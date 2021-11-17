f757d1f0866169bad0b61056a970fe7e05d49bb4
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-11-18 03:50:34 +0800

    修改front-end/vue/vue2-style-scoped demo

--- front-end/vue/vue2-style-scoped/src/views/Home.vue
+++ front-end/vue/vue2-style-scoped/src/views/Home.vue
@@ -30,6 +30,7 @@ Vue中的scoped的实现原理以及scoped穿透的用法
 https://blog.csdn.net/sinat_17775997/article/details/83416617
 */
 // 方法1
+// NOTE: 这样不会造成全局el-image样式污染问题，如果使用style标签不带scoped会造成全局el-image演示污染问题
 .el-image {
   width: 160px;
   height: 160px;
