b418d8ea2b6d71fefd1637d1896bc87eb733509d
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-12-23 11:22:24 +0800

    spring-boot-quartz demo配置执行线程数

--- spring-boot-quartz/src/main/java/com/future/demo/quartz/ConfigQuartz.java
+++ spring-boot-quartz/src/main/java/com/future/demo/quartz/ConfigQuartz.java
@@ -1,8 +1,15 @@
 package com.future.demo.quartz;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import java.util.Properties;
 
 /**
  * Disable @EnableScheduling on Spring Tests
@@ -13,5 +20,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 )
 @Configuration
 @EnableScheduling
-public class ConfigQuartz {
+public class ConfigQuartz implements SchedulingConfigurer {
+    // 配置spring scheduling核心执行线程，spring默认执行线程数为1
+    // https://stackoverflow.com/questions/29796651/what-is-the-default-scheduler-pool-size-in-spring-boot
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(5);
+        taskScheduler.initialize();
+        taskRegistrar.setTaskScheduler(taskScheduler);
+    }
 }
