9f2e4355c80adbf1e08c0ad4a14909af541bb395
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-02-01 22:28:06 +0800

    python基础学习demo

---
+++ python/range/main.py
@@ -0,0 +1,15 @@
+varRange = range(10)
+print(list(varRange))
+
+varRange = range(1, 10)
+print(list(varRange))
+
+varRange = range(1, 10, 2)
+print(list(varRange))
+
+# in和not in
+print(0 in varRange)
+print(1 in varRange)
+
+print(0 not in varRange)
+print(1 not in varRange)
