ef17c5e6c0bfea8c8f2ce3ad0452749a34491441
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-02-03 13:20:32 +0800

    python新增模块demo

---
+++ python/module/module-demo.py
@@ -0,0 +1,34 @@
+# 导入math模块
+print("导入math模块".center(50, "-"))
+import math
+
+print("math.pi={0}".format(math.pi))
+print("math.pow(2, 3)={0}".format(math.pow(2, 3)))
+
+# 导入别名
+print()
+print("导入math模块别名".center(50, "-"))
+import math as mathlib
+
+print("mathlib.pi={0}".format(mathlib.pi))
+print("mathlib.pow(2, 3)={0}".format(mathlib.pow(2, 3)))
+
+# 导入函数、变量、类
+print()
+print("导入函数、变量、类".center(50, "-"))
+
+from math import pi
+from math import pow
+
+print("pi={0}".format(pi))
+print("pow(2, 3)={0}".format(pow(2, 3)))
+
+# 导入自定义模块
+print()
+print("导入自定义模块".center(50, "-"))
+
+import module_self_defined
+
+print("module_self_defined.add(1, 2)={0}".format(module_self_defined.add(1, 2)))
+varClassA = module_self_defined.ClassA()
+varClassA.method1()
