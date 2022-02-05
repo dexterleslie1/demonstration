6bf669b8635cb0f7e4f2f64ad56426c74352c463
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-02-05 22:26:07 +0800

    python-basic新增判断目录或者文件是否存在demo

--- python/python-basic/file_and_directory.py
+++ python/python-basic/file_and_directory.py
@@ -10,7 +10,21 @@ except:
     pass
 
 # 打开和关闭文件
+import traceback
+varFile = None
+try:
+    varFile = open("tmp1/file_temporary.tmp", "w")
+    varFile.write("文件内容")
+except:
+    traceback.print_exc()
+finally:
+    if varFile is not None:
+        varFile.close()
 
-varFile = open("tmp1/file_temporary.tmp", "w")
-varFile.write("文件内容")
-varFile.close()
+# 判断目录是否已存在
+assert os.path.exists("tmp1")
+assert not os.path.exists("tmp111")
+
+assert os.path.exists("tmp1/file_temporary.tmp")
+assert not os.path.exists("tmp1/file_temporary.tmp1")
+
