bb4f9fc3c85340f919552e4dd576e86183e2ac0d
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-02-08 15:52:27 +0800

    新增elk-elasticsearch demo

---
+++ elk/elk-elasticsearch/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 编译elasticsearch
+docker build --tag docker.118899.net:10001/demo-elk-elasticsearch --file Dockerfile-elasticsearch .
+
+# 编译elasticsearch-head
+docker build --tag docker.118899.net:10001/demo-elk-elasticsearch-head --file Dockerfile-elasticsearch-head .
