79560903efd29078ebb52f311f7e72f5582d44b9
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-05-06 15:58:39 +0800

    改进maven-tomcat-plugin-servlet-example demo

---
+++ maven-tomcat-plugin-servlet-example/README.md
@@ -0,0 +1,16 @@
+# 演示tomcat7-maven-plugin使用
+
+## maven插件 tomcat7-maven-plugin 的使用
+
+> [maven插件 tomcat7-maven-plugin 的使用](https://blog.csdn.net/xiaojin21cen/article/details/78570254)
+
+## 运行项目
+
+```shell script
+# 运行tomcat7-maven-plugin
+mvn tomcat7:run
+
+# 发布war
+mvn clean package
+```
+
--- maven-tomcat-plugin-servlet-example/src/main/java/com/future/study/maven/tomcat/plugin/servlet/SimpleServlet.java
+++ maven-tomcat-plugin-servlet-example/src/main/java/com/future/study/maven/tomcat/plugin/servlet/SimpleServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * @author dexterleslie@gmail.com
@@ -16,7 +17,16 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().write("Hello World! Maven Web Project Example.");
+    	String p1 = req.getParameter("p1");
+    	if(p1==null || p1.trim().length()<=0) {
+    		try {
+				Thread.sleep(15000);
+			} catch (InterruptedException e) {
+				//
+			}
+    	}
+        resp.setContentType("text/plain;charset=utf-8");
+        Date date = new Date();
+        resp.getWriter().write("系统当前时间:" + date);
     }
 }
