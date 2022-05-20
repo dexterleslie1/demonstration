6e10c03f1341a269d34d5f344c30cfafe54da796
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-05-20 14:29:29 +0800

    shell-scripting demo新增tomcat-service-restart.sh演示定时重启tomcat、nginx等服务

---
+++ shell-scripting/tomcat-service-restart.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# tomcat、nginx等服务重启脚本
+# 编辑crontab -uroot -e填写如下内容
+# 0 2 */5 * * /data/service-restart.sh
+
+varTomcatUser=root
+varTomcatDir=/data/tomcat
+
+echo "`date` 停止nginx。。。"
+service nginx stop
+
+# 关闭tomcat
+echo "`date` 关闭tomcat。。。"
+sudo -u $varTomcatUser sh $varTomcatDir/bin/shutdown.sh 1>/dev/null
+
+# 检查tomcat是否已经关闭
+while curl --output /dev/null --silent http://localhost:8080/;
+do
+ echo "`date` 等待tomcat关闭中。。。"
+ sleep 1
+done
+
+# 启动tomcat
+echo "`date` 启动tomcat。。。"
+sudo -u $varTomcatUser sh $varTomcatDir/bin/startup.sh 1>/dev/null
+
+# 检查tomcat是否已经启动
+while ! curl --output /dev/null --silent http://localhost:8080/;
+do
+ echo "`date` 等待tomcat启动中。。。"
+ sleep 1
+done
+
+echo "`date` 启动nginx。。。"
+service nginx start
+
+
