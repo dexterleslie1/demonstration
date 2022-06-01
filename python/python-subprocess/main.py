fc5bd76985552b66b0e0ef4c0ec1316b78cf67a3
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-06-01 21:43:19 +0800

    python-subprocess run方式演示check=True用法

--- python/python-subprocess/main.py
+++ python/python-subprocess/main.py
@@ -1,4 +1,5 @@
 import subprocess
+import traceback
 
 # call方法使用，执行成功返回0,执行失败返回非0
 # call方法不能读取stdout和stderr
@@ -19,5 +20,13 @@ assert varResult.returncode == 0, "期望命令执行成功"
 varResult = subprocess.run("ls1 ~", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
 assert varResult.returncode != 0, "期望命令执行失败"
 # print("varResult: ", varResult)
+
+# check=True表示命令执行错误抛出异常
+try:
+    subprocess.run("ls1 ~", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+except subprocess.CalledProcessError:
+    pass
+else:
+    raise Exception("期望异常没有抛出")
 
 # Popen方法暂时未用到，call和run方法已经够用
