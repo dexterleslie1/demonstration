bef1cd0712ff63ad9c59ebfbc7be684cf8cd06b0
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-06-17 19:10:18 +0800

    改进robot-detection-redis-cluster demo

--- captcha/robot-detection-redis-cluster/src/main/java/com/future/demo/robot/detection/CaptchaCacheServiceImpl.java
+++ captcha/robot-detection-redis-cluster/src/main/java/com/future/demo/robot/detection/CaptchaCacheServiceImpl.java
@@ -1,10 +1,13 @@
 package com.future.demo.robot.detection;
 
+import com.dex.hm.common.utils.web.RequestUtils;
 import com.yyd.common.ddos.AbstractCaptchaCacheService;
+import org.apache.commons.lang.StringUtils;
 import org.redisson.api.RedissonClient;
 import redis.clients.jedis.JedisCluster;
 
 import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -35,5 +38,24 @@ public class CaptchaCacheServiceImpl extends AbstractCaptchaCacheService {
     @Override
     public List<String> getIgnoreUriList() {
         return Collections.singletonList(UriEnable);
+    }
+
+    @Override
+    public String getClientIp(HttpServletRequest request) {
+        String clientIp = RequestUtils.getString(request, "x-client-ip");
+        if(StringUtils.isBlank(clientIp)) {
+            clientIp = super.getClientIp(request);
+        }
+        return clientIp;
+    }
+
+    @Override
+    public boolean isGetCaptchaResponseWithResult() {
+        return true;
+    }
+
+    @Override
+    public boolean enableVerifyDelay() {
+        return false;
     }
 }
