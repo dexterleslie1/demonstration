39e499a0af949bbafc7c16b92a31ed7f1e3ff5dc
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-11-08 10:54:05 +0800

    demo-c++/demo-program-memory新增动态内存分配和回收演示

--- demo-c++/demo-c++/demo-program-memory/main.cpp
+++ demo-c++/demo-c++/demo-program-memory/main.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <assert.h>
 
 using namespace std;
 
@@ -46,6 +48,67 @@ int main() {
 	int *heap_b = new int(11);
 	cout << "堆区变量a地址：" << heap_a << endl;
 	cout << "堆区变量b地址：" << heap_b << endl;
+
+	/* 动态内存分配 */
+	// https://blog.csdn.net/qq_53086187/article/details/124527767
+	
+	// 分配一个大小为size字节的内存块，返回该内存块开头的地址
+	// 分配一个10个整型大小的内存块，将开头地址传递给p。
+	// 如果malloc函数分配内存失败则会返回一个空指针。所以使用时需要进行判断。
+	cout << endl << "---------------演示使用malloc分配内存" << endl;
+	int byteCount = 2;
+	int *p1 = (int *)malloc(byteCount * sizeof(int));
+	cout << "malloc分配的内存地址：" << p1 << endl;
+	if(p1 != NULL) {
+		for(int i = 0; i < byteCount; i++) {
+			*(p1 + i) = i;
+		}
+
+		for(int i = 0; i < byteCount; i++) {
+			cout << "*(p1 + " << i << ")=" << *(p1 + i) << endl; 
+		}
+	}
+
+	cout << endl << "-----------------演示使用calloc分配内存" << endl;
+	p1 = (int *)calloc(byteCount, sizeof(int));
+	cout << "calloc分配的内存地址：" << p1 << endl;
+	if(p1 != NULL) {
+		for(int i = 0; i < byteCount; i++) {
+			*(p1 + i) = i;
+		}
+
+		for(int i = 0; i < byteCount; i++) {
+			cout << "*(p1 + " << i << ")=" << *(p1 + i) << endl; 
+		}
+	}
+
+	cout << endl << "---------------------演示使用free释放分配内存" << endl;
+	// 通过动态内存分配的内存空间在不使用时要进行释放。
+	free(p1);
+	p1 = NULL;
+
+	cout << endl << "-------------------演示使用realloc调整分配内存大小" << endl;
+	// 当我们开辟一块空间后不能够满足需求时，可以使用realloc函数进行大小调整
+	// 更改ptr指向的内存块的大小为size，如果ptr为空指针则相当于malloc开辟空间。
+	// realloc函数调整空间时分为两种情况。
+	// 第一：如果原空间后的内存空间足够分配，则直接进行空间追加。
+	// 第二：如果原空间后的内存空间不足分配，则重新开辟一块空间，将开头地址返回。
+	// 如果内存调整失败则会返回一个空指针。所以使用前需要进行判断。
+	p1 = (int *)calloc(byteCount, sizeof(int));
+	for(int i = 0; i < byteCount; i++) {
+		*(p1 + i) = i;
+	}
+	for(int i = 0; i < byteCount; i++) {
+		cout << "*(p1 + " << i << ")=" << *(p1 + i) << endl; 
+	}
+	byteCount = byteCount * 2;
+	int *p2 = (int *)realloc(p1, byteCount * sizeof(int));
+	cout << "realloc分配的内存地址：" << p1 << endl;
+	assert(p1 == p2);
+	for(int i = 0; i < byteCount; i++) {
+		cout << "*(p1 + " << i << ")=" << *(p1 + i) << endl; 
+	}
+
 
 	system("pause");
 
