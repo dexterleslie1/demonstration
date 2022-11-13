5f7e12d278d67695173aca06937384b807e7f89a
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-11-13 12:46:09 +0800

    demo-shared-library复制libmylib.so到/lib/

--- demo-make/demo-shared-library/demo-executable/build.sh
+++ demo-make/demo-shared-library/demo-executable/build.sh
@@ -9,7 +9,16 @@
 
 rm -f test
 
+rm -rf lib
+rm -rf include
+
+mkdir lib
+mkdir include
+
 cp ../demo-library/libmylib.so ./lib/
 cp ../demo-library/mylib.h ./include/
 
 gcc main.c -l mylib -L ./lib -o test -I ./include/
+
+# 发布libmylib.so到/lib/，否则在运行 test 程序时报告找不到动态链接库错误
+sudo cp lib/libmylib.so /lib/
