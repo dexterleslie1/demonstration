7a2049d99603779ee585bf30f3848b198e0c5828
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-12-01 21:06:01 +0800

    demo-c++新增demo-goto

---
+++ demo-c++/demo-c++-visual-studio/demo-goto/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "line 1" << endl;
+
+	cout << "line 2" << endl;
+
+	goto _l_goto;
+
+	cout << "line 3" << endl;
+	cout << "line 4" << endl;
+
+	_l_goto:
+
+	cout << "line 5" << endl;
+
+	system("pause");
+	return 0;
+}
