9865c5f96d965cb82e961701a036b343e663fa3c
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-12-01 20:59:02 +0800

    demo-c++新增demo-while

---
+++ demo-c++/demo-c++-visual-studio/demo-while/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void test_do_while() {
+	int counter = 0;
+	do {
+		cout << "counter=" << counter << endl;
+		counter++;
+	} while(counter < 10);
+}
+
+int main(){
+	test_do_while();
+
+	system("pause");
+	return 0;
+}
