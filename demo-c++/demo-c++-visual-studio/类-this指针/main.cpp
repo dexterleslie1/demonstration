298140cbf0e51e5467c88ce5bd63e5d8d3d20a30
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-12-03 15:35:26 +0800

    demo-c++新增 "类-this指针" 和 "类-常函数和常对象"

---
+++ demo-c++/demo-c++-visual-studio/类-this指针/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Person {
+public:
+	int age;
+
+	Person():age(10) {
+	}
+
+	Person& addAge(const Person &person) {
+		this->age += person.age;
+		// 返回本对象
+		return *this;
+	}
+};
+
+int main() {
+	Person person1;
+	Person person2;
+	person2.addAge(person1).addAge(person1);
+	cout << "person2.age=" << person2.age << endl;
+
+	system("pause");
+	return 0;
+}
