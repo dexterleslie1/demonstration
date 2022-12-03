07539b86c27b2c36d48bd2f923581600d4e0e062
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-12-03 20:18:55 +0800

    demo-c++新增 "类-声明和定义分文件"

---
+++ demo-c++/demo-c++-visual-studio/类-声明和定义分文件/Circle.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "Point.h"
+#include <iostream>
+
+using namespace std;
+
+class Circle {
+private:
+	int r;
+	Point center;
+
+public:
+	void setR(int r);
+	void setCenter(Point center);
+	int getR();
+	Point getCenter();
+
+	void print();
+};
---
+++ demo-c++/demo-c++-visual-studio/类-声明和定义分文件/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Circle.h"
+
+int main() {
+	Point point;
+	point.setX(100);
+	point.setY(101);
+
+	Circle circle;
+	circle.setR(200);
+	circle.setCenter(point);
+	circle.print();
+
+	system("pause");
+	return 0;
+}
