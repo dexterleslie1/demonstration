1b55db7c85990160fc87beab2de72d6a9aac2f26
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-12-09 21:06:53 +0800

    json-demo.py新增包含datetime对象的JSON转换

--- python/python-basic/json-demo.py
+++ python/python-basic/json-demo.py
@@ -3,6 +3,7 @@
 # https://www.cnblogs.com/miqi1992/p/8081244.html
 
 import json
+import datetime
 
 # python对象转换为JSON字符串，dumps函数使用
 varDict = {"name": "Dexter", "age": 30}
@@ -28,3 +29,20 @@ assert varDictExpected == varDict
 varListExpected = [1, 2, 3]
 varList = json.loads(varJSONList)
 assert varListExpected == varList
+
+# 转换包含datetime对象的JSON对象
+# https://blog.csdn.net/t8116189520/article/details/88657533
+
+
+class DateEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj,datetime.datetime):
+            return obj.strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            return json.JSONEncoder.default(self,obj)
+
+
+timeNow = datetime.datetime.now()
+JSONObject = {"datetimeObject": timeNow}
+JSON = json.dumps(JSONObject, cls=DateEncoder)
+print("包含datetime对象的JSON对象被转换为JSON字符串后 ", JSON);
