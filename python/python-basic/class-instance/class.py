9bd02a3272bdded4238056d480e606f7d0a1cfa8
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-12-10 19:19:50 +0800

    更新python/class.py

--- python/python-basic/class-instance/class.py
+++ python/python-basic/class-instance/class.py
@@ -1,10 +1,9 @@
-
 class Student:
-    # 类属性
+    # 类属性: 所有类或者类对象共享一个类属性
     classProperty = "类属性classProperty"
 
     # 构造函数
-    def __init__(self, name = "Dexter", age = 30):
+    def __init__(self, name="Dexter", age=30):
         self.name = name
         self.age = age
         # 私有对象属性
@@ -29,38 +28,17 @@ class Student:
 
     pass
 
-print("类对象信息".center(50, "-"))
-print("id:{0},type:{1},instance:{2}".format(id(Student), type(Student), Student))
 
-print()
-print("对象操作".center(50, "-"))
-# 创建对象
-student = Student()
-# 调用实例方法
-student.info()
-# 另外一种方法调用对象方法，等价于student.info()
-Student.info(student)
-# 打印对象属性
-print("你是{0}，今年{1}岁".format(student.name, student.age))
+class ClassPrivateMember:
+    def __init__(self, a):
+        self.__a = a
+        pass
 
-student = Student("Dexterleslie")
-student.info()
+    def show(self):
+        print("私有成员self.__a=", self.__a)
 
-student = Student("Dexterleslie", 31)
-student.info()
-student.infoSex()
+    pass
 
-print()
-print("类操作".center(50, "-"))
-# 类属性访问
-print("类属性Student.classProperty:{0}".format(Student.classProperty))
-
-# 类方法和静态方法调用
-Student.classMethod()
-Student.staticMethod()
-
-print()
-print("类继承".center(50, "-"))
 
 # 单继承
 class Person(object):
@@ -71,16 +49,31 @@ class Person(object):
     def info(self):
         print("我是{0}，今年{1}岁".format(self.name, self.age))
 
+
 class StudentSub(Person):
     def __init__(self, name, age, score):
+        # 定义子类时必须在其构造函数中调用父类构造函数
         super().__init__(name, age)
+
         self.score = score
 
+    # 方法重写
     def info(self):
+        # 调用父级函数
+        super().info()
         print("我是{0}，今年{1}岁，考试{2}分".format(self.name, self.age, self.score))
 
-student = StudentSub("Dexter1", 20, 98)
-student.info()
+
+class TeacherSub(Person):
+    def __init__(self, name, age, title):
+        super().__init__(name, age)
+        self.title = title
+
+    # 方法重写
+    def info(self):
+        super().info()
+        print("教师职称：", self.title)
+
 
 # 多继承
 class A:
@@ -89,16 +82,134 @@ class A:
 
     pass
 
+
 class B:
     def methodB(self):
         print("你调用方法B")
 
     pass
 
+
 class C(A, B):
+    pass
+
+
+def test_method():
+    print("测试动态绑定方法")
+    pass
+
+# https://blog.csdn.net/weixin_42147967/article/details/123394838
+from abc import abstractmethod, ABCMeta
+
+
+class Base(metaclass=ABCMeta):
+    @abstractmethod
+    def method1(self):
+        pass
+
+    def method2(self):
+        print("抽象类Base方法2实现")
 
     pass
 
-varC = C()
-varC.methodA()
-varC.methodB()
+
+class Children(Base):
+    # 如果不实现父类抽象方法会如下错误
+    # TypeError: Can't instantiate abstract class Children with abstract methods method1
+    def method1(self):
+        print("子类Children方法1实现")
+
+    pass
+
+
+if __name__ == "__main__":
+    print("类对象信息".center(50, "-"))
+    print("id:{0},type:{1},instance:{2}".format(id(Student), type(Student), Student))
+
+    print()
+    print("对象操作".center(50, "-"))
+    # 创建对象
+    student = Student()
+    # 调用实例方法
+    student.info()
+    # 另外一种方法调用对象方法，等价于student.info()
+    Student.info(student)
+
+    # 打印对象属性
+    print("你是{0}，今年{1}岁".format(student.name, student.age))
+
+    # 使用带参数构造函数创建对象
+    student = Student("Dexterleslie")
+    student.info()
+
+    student = Student("Dexterleslie", 31)
+    student.info()
+    student.infoSex()
+
+    print()
+    print("类操作".center(50, "-"))
+    # 类属性访问
+    print("类属性Student.classProperty:{0}".format(Student.classProperty))
+    # NOTE: 只能使用Student.classProperty修改类属性，
+    # 不能使用student.classProperty修改，否则是给student对象动态绑定属性
+    Student.classProperty = "1"
+    print("使用Student.classProperty修改后的student.classProperty：", student.classProperty)
+    print("使用Student.classProperty修改后的Student.classProperty：", Student.classProperty)
+
+    # 类方法和静态方法调用，目前理解静态方法和类方法区别不大
+    # https://blog.csdn.net/yin20100522/article/details/94455982
+    Student.classMethod()
+    Student.staticMethod()
+
+    print()
+    print("为对象动态绑定属性和方法".center(50, "-"))
+    student1 = Student()
+    student2 = Student()
+    student1.prop1 = "动态绑定属性"
+    print("student1动态绑定属性prop1=", student1.prop1)
+    try:
+        print("student2动态绑定属性prop1=", student2.prop1)
+        assert 1 != 1
+    except AttributeError:
+        # student2没有prop2动态绑定prop1属性
+        pass
+
+    # 动态绑定方法
+    student1.test_method = test_method
+    student1.test_method()
+    try:
+        student2.test_method()
+        assert 1 != 1
+    except AttributeError:
+        # student2没有prop2动态绑定prop1属性
+        pass
+
+    print()
+    print("类私有成员".center(50, "-"))
+    classPrivateMember = ClassPrivateMember(10)
+    classPrivateMember.show()
+
+    # 单继承和多继承
+    print()
+    print("类继承".center(50, "-"))
+    student = StudentSub("Dexter1", 20, 98)
+    student.info()
+    varC = C()
+    varC.methodA()
+    varC.methodB()
+
+    # 方法重写
+    print()
+    print("方法重写".center(50, "-"))
+    student = StudentSub("dexter2", 21, 99)
+    student.info()
+    teacher = TeacherSub("teacher1", 33, "特级教师")
+    teacher.info()
+
+    print()
+    print("抽象类".center(50, "-"))
+    children = Children()
+    children.method1()
+    children.method2()
+
+    pass
