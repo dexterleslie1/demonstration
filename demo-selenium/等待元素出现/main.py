c66a20c5d8e9b170234095a3a4827ec087c9e390
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-12-18 09:44:26 +0800

    demo-selenium等待元素出现新增显式等待元素出现

--- demo-selenium/等待元素出现/main.py
+++ demo-selenium/等待元素出现/main.py
@@ -3,6 +3,8 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 def test_implicitly_wait_element():
@@ -24,5 +26,19 @@ def test_implicitly_wait_element():
     pass
 
 
+def test_explicitly_wait_element():
+    driver = webdriver.Firefox()
+    driver.get("https://www.baidu.com")
+    wait = WebDriverWait(driver=driver, timeout=5)
+    # https://blog.csdn.net/dingding_ting/article/details/116768566
+    wait.until(expected_conditions.title_is("百度一下，你就知道"))
+
+    # https://www.jb51.net/article/219686.htm
+    wait.until(expected_conditions.presence_of_element_located((By.XPATH, "//input[@id='su']")))
+
+    driver.quit()
+
+
 if __name__ == "__main__":
-    test_implicitly_wait_element()
+    # test_implicitly_wait_element()
+    test_explicitly_wait_element()
