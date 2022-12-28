ea7fda01cc2a204951bf9e226544c60c7869daca
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-12-28 15:19:25 +0800

    demo-golang/file-and-directory.go新增path.Join测试

--- demo-golang/file-and-directory.go
+++ demo-golang/file-and-directory.go
@@ -1,25 +1,27 @@
 package main
 
 import (
-	"os"
-	"fmt"
 	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
 )
 
 func main() {
 	// 用户home目录获取
 	// https://stackoverflow.com/questions/7922270/obtain-users-home-directory
 	homeDirectory, err := os.UserHomeDir()
-    if err != nil {
-        panic("无法获取用户home目录：" + err.Error())
-    }
+	if err != nil {
+		panic("无法获取用户home目录：" + err.Error())
+	}
 
 	// https://stackoverflow.com/questions/46748636/how-to-create-new-file-using-go-script
 	// 写文件，如果文件不存在创建并写入，如果文件存在则直接覆盖写入
-	err = os.WriteFile(homeDirectory + "/.1.txt", []byte("Hello\n"), 0755)
-    if err != nil {
-        fmt.Printf("Unable to write file: %v", err)
-    }
+	err = os.WriteFile(homeDirectory+"/.1.txt", []byte("Hello\n"), 0755)
+	if err != nil {
+		fmt.Printf("Unable to write file: %v", err)
+	}
 
 	// https://stackoverflow.com/questions/12518876/how-to-check-if-a-file-exists-in-go
 	// 判断文件已经存在
@@ -30,12 +32,30 @@ func main() {
 	// https://www.geeksforgeeks.org/how-to-delete-or-remove-a-file-in-golang/
 	// 删除文件
 	e := os.Remove(homeDirectory + "/.1.txt")
-    if e != nil {
-        panic("删除文件" + homeDirectory + "/.1.txt失败")
-    }
+	if e != nil {
+		panic("删除文件" + homeDirectory + "/.1.txt失败")
+	}
 
 	// 判断文件不存在
 	if _, err := os.Stat(homeDirectory + "/.1.txt"); err == nil {
 		panic("预期文件" + homeDirectory + "/.1.txt存在")
+	}
+
+	/* 测试path.Join */
+	fullPath := path.Join("1", "2", "3")
+	fmt.Println("完整路径:", fullPath)
+
+	/* 测试mv命令 */
+	// https://blog.csdn.net/whatday/article/details/109315495
+	oldLocation := "./.test.txt"
+	newLocation := "./.newtest.txt"
+	var mvCmd *exec.Cmd = exec.Command("mv", oldLocation, newLocation)
+	// CombinedOutput表示标准输出和错误输出合成
+	// https://stackoverflow.com/questions/1877045/how-do-you-get-the-output-of-a-system-command-in-go
+	out, err := mvCmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("mv命令失败", err, string(out))
+	} else {
+		fmt.Println("成功移动./.test.txt到./.newtest.txt")
 	}
 }
