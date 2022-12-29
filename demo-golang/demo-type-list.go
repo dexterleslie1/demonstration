2b57fcb29b7d54213312353b8d84e3e7f30d7851
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-12-29 12:33:59 +0800

    新增demo-golang/demo-type-map.go

--- demo-golang/demo-type-list.go
+++ demo-golang/demo-type-list.go
@@ -9,14 +9,14 @@ import (
 // https://blog.csdn.net/cuit_DreamSaga/article/details/107450434
 func main() {
 	// 数组长度
-	var arr1 = [10] int {1, 2, 3}
+	var arr1 = [10]int{1, 2, 3}
 	fmt.Println("长度：" + strconv.Itoa(len(arr1)))
 
 	// 打印数组
 	fmt.Println(arr1)
 
 	// 遍历数组
-	for i:=0; i<len(arr1); i++ {
+	for i := 0; i < len(arr1); i++ {
 		fmt.Println("index=" + strconv.Itoa(i) + ",value=" + strconv.Itoa(arr1[i]))
 	}
 }
