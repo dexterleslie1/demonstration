bd52728c9ad5fe27dffb79ae5554377b36af6e73
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-03-07 10:32:35 +0800

    改进spring-amqp-rabbitmq-delayed-message-exchange demo支持x-max-retries

---
+++ rabbitmq-examples/spring-amqp-rabbitmq-delayed-message-exchange/src/main/java/com/future/demo/ConfigDemoMq.java
@@ -0,0 +1,87 @@
+package com.future.demo;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Configuration
+@Slf4j
+public class ConfigDemoMq {
+    public static final String BusinessPrefix = "demo-test-delayed";
+    public static final String ExchangeName = BusinessPrefix + "-x";
+    public static final String QueueName = BusinessPrefix + "-q";
+    public static final String RoutingKey = "routingKey1";
+    private static final String HeaderXMaxRetries = "x-max-retries";
+
+    private final AtomicInteger counter = new AtomicInteger();
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    @RabbitListener(queues = ConfigDemoMq.QueueName)
+    public void receiveMessage(String message, Message messageO, Channel channel) throws Exception {
+        try {
+            log.info("Received <" + message + ">");
+            counter.incrementAndGet();
+
+            boolean b = true;
+            if(b) {
+                throw new Exception("测试异常");
+            }
+        } catch (Exception ex) {
+            Integer xMaxRetries = messageO.getMessageProperties().getHeader(HeaderXMaxRetries);
+            if(xMaxRetries == null) {
+                xMaxRetries = 0;
+            }
+
+            messageO.getMessageProperties().setHeader(HeaderXMaxRetries, xMaxRetries + 1);
+
+            log.info("x-max-retries={}", xMaxRetries);
+
+            // 最多重试1次
+            if(xMaxRetries < 1) {
+                this.rabbitTemplate.convertAndSend(ConfigDemoMq.ExchangeName, ConfigDemoMq.RoutingKey, messageO, MessagePostProcessortVariable);
+            }
+        } finally {
+            channel.basicAck(messageO.getMessageProperties().getDeliveryTag(), false);
+        }
+    }
+
+    final static long Delay = 3000;
+    public final static MessagePostProcessor MessagePostProcessortVariable = message -> {
+        message.getMessageProperties().setHeader("x-delay", Delay);
+        return message;
+    };
+
+    public int getCount() {
+        return this.counter.get();
+    }
+
+    @Bean
+    public Queue queue() {
+        return QueueBuilder.nonDurable(ConfigDemoMq.QueueName).build();
+    }
+
+    // 配置默认的交互机
+    @Bean
+    public CustomExchange customExchange() {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-delayed-type", "direct");
+        // 参数二为类型：必须是x-delayed-message
+        return new CustomExchange(ExchangeName, "x-delayed-message", false, false, args);
+    }
+
+    // 绑定队列到交换器
+    @Bean
+    public Binding binding(Queue queue, CustomExchange customExchange) {
+        return BindingBuilder.bind(queue).to(customExchange).with(RoutingKey).noargs();
+    }
+}
--- rabbitmq-examples/spring-amqp-rabbitmq-delayed-message-exchange/src/test/java/com/future/demo/ApplicationTests.java
+++ rabbitmq-examples/spring-amqp-rabbitmq-delayed-message-exchange/src/test/java/com/future/demo/ApplicationTests.java
@@ -9,38 +9,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Date;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes={Application.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class ApplicationTests {
-    final static long Delay = 3000;
-
     @Autowired
     private AmqpTemplate rabbitTemplate;
     @Autowired
-    private Receiver receiver;
+    private ConfigDemoMq configDemoMq;
 
     @Test
     public void test1() throws InterruptedException {
-        int totalCount = 5;
+        int totalCount = 1;
 
         for(int i=0; i<totalCount; i++){
-            this.rabbitTemplate.convertAndSend(Config.ExchangeName, "routingKey1", "888", MessagePostProcessortVariable);
+            this.rabbitTemplate.convertAndSend(ConfigDemoMq.ExchangeName, ConfigDemoMq.RoutingKey, "888", ConfigDemoMq.MessagePostProcessortVariable);
         }
 
         Thread.sleep(1000);
 
         // 未收到delay消息
-        Assert.assertEquals(0, receiver.getCount());
+        Assert.assertEquals(0, configDemoMq.getCount());
 
         Thread.sleep(2500);
         // 收到所有delay消息
-        Assert.assertEquals(totalCount, receiver.getCount());
-    }
+        Assert.assertEquals(totalCount, configDemoMq.getCount());
 
-    private final static MessagePostProcessor MessagePostProcessortVariable = message -> {
-        message.getMessageProperties().setHeader("x-delay", Delay);
-        return message;
-    };
+        // 最多重试一次
+        Thread.sleep(5000);
+        Assert.assertEquals(totalCount + 1, configDemoMq.getCount());
+    }
 }
