6b7a5da9aff859d1fd8be424c1c109834566754b
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-03-26 07:56:18 +0800

    忽略提交

--- elk/build.sh
+++ elk/build.sh
@@ -2,6 +2,3 @@
 
 # 编译elasticsearch
 docker build --tag docker.118899.net:10001/yyd-public/demo-elk-elasticsearch --file Dockerfile-elasticsearch .
-
-## 编译elasticsearch-head
-#docker build --tag docker.118899.net:10001/yyd-public/demo-elk-elasticsearch-head --file Dockerfile-elasticsearch-head .
