d53bf3300a3382d091fe46bfccd4e2d9af280326
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-04-03 21:07:55 +0800

    shiro-standalone新增authorization测试用例

--- demo-shiro/shiro-standalone/src/test/java/com/future/demo/Tests.java
+++ demo-shiro/shiro-standalone/src/test/java/com/future/demo/Tests.java
@@ -5,6 +5,7 @@ import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
@@ -37,5 +38,41 @@ public class Tests {
 
         authenticationToken = new UsernamePasswordToken("zhangsan", "zs");
         subject.login(authenticationToken);
+    }
+
+    @Test
+    public void testAuthorization() {
+        IniSecurityManagerFactory factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+        AuthenticationToken authenticationToken = new UsernamePasswordToken("zhangsan11", "zs");
+        try {
+            subject.login(authenticationToken);
+            Assert.fail("预期异常没有抛出");
+        }  catch (UnknownAccountException ignored) {
+
+        }
+
+        authenticationToken = new UsernamePasswordToken("zhangsan", "zs11");
+        try {
+            subject.login(authenticationToken);
+            Assert.fail("预期异常没有抛出");
+        }  catch (IncorrectCredentialsException ignored) {
+
+        }
+
+        authenticationToken = new UsernamePasswordToken("zhangsan", "zs");
+        subject.login(authenticationToken);
+        Assert.assertFalse(subject.hasRole("role1111"));
+        Assert.assertTrue(subject.hasRole("role1"));
+        Assert.assertFalse(subject.isPermitted("user:update"));
+        Assert.assertTrue(subject.isPermitted("user:delete"));
+        try {
+            subject.checkPermission("user:update");
+            Assert.fail("预期异常没有抛出");
+        } catch (AuthorizationException ignored) {
+        }
+        subject.checkPermission("user:insert");
     }
 }
