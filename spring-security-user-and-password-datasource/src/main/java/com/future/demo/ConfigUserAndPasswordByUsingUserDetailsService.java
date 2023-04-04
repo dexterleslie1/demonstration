a709a3d7a05b8f47ee3ff34d843221007bab0b0d
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-04-04 21:11:42 +0800

    改进spring-security-user-and-password-datasource

--- spring-security-user-and-password-datasource/src/main/java/com/future/demo/ConfigUserAndPasswordByUsingUserDetailsService.java
+++ spring-security-user-and-password-datasource/src/main/java/com/future/demo/ConfigUserAndPasswordByUsingUserDetailsService.java
@@ -1,4 +1,4 @@
-package com.future.demo.spring.security;
+package com.future.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
