a02df5fcea40d1d4b81318cf76bccea9a6a07bca
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-04-28 13:51:03 +0800

    demo-modelmapper新增测试用例演示List<UserModel>转换为List<UserVo>

--- demo-modelmapper/src/test/java/com/future/demo/Tests.java
+++ demo-modelmapper/src/test/java/com/future/demo/Tests.java
@@ -3,6 +3,11 @@ package com.future.demo;
 import org.junit.Assert;
 import org.junit.Test;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Tests {
     @Test
@@ -14,5 +19,13 @@ public class Tests {
         UserVo userVo = modelMapper.map(userModel, UserVo.class);
         Assert.assertEquals(userModel.getId(), userVo.getId());
         Assert.assertEquals(userModel.getName(), userVo.getName());
+
+        // 测试List<UserMode>转换为List<UserVo>对象
+        List<UserModel> userModelList = new ArrayList<>();
+        userModelList.add(userModel);
+        List<UserVo> userVoList = userModelList.stream().map(o-> modelMapper.map(o, UserVo.class)).collect(Collectors.toList());
+        Assert.assertEquals(1, userVoList.size());
+        Assert.assertEquals(userModel.getId(), userVoList.get(0).getId());
+        Assert.assertEquals(userModel.getName(), userVoList.get(0).getName());
     }
 }
