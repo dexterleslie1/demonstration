79e7ee7b47260a357d789b64cde0721dc8164e46
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-05-11 20:45:37 +0800

    忽略提交

--- spring-boot-propertysource/src/main/java/com/future/demo/spring/boot/test/Config.java
+++ spring-boot-propertysource/src/main/java/com/future/demo/spring/boot/test/Config.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+// 加载自定义properties文件作为默认值
 @PropertySource("classpath:my-test.properties")
 public class Config {
 }
