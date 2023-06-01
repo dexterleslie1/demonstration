cbc1bb9640d1cc5170a2f67992f90dbcc08da48d
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-06-01 13:41:55 +0800

    font-end/jquery/ajax添加jquery ajax全局异常处理demo

--- front-end/jquery/ajax/src/main/java/com/future/demo/jquery/ajax/DemoDatumController.java
+++ front-end/jquery/ajax/src/main/java/com/future/demo/jquery/ajax/DemoDatumController.java
@@ -1,6 +1,7 @@
 package com.future.demo.jquery.ajax;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,12 +12,18 @@ import java.util.Map;
 @RequestMapping("/api/v1/")
 public class DemoDatumController {
     @GetMapping("get")
-    public Map<String, String> get(HttpServletRequest request) {
+    public ResponseEntity<Map<String, String>> get(HttpServletRequest request,
+                                                   // exception=true 返回状态码为400
+                                                   @RequestParam(value = "exception", defaultValue = "false") boolean exception) {
         Map<String, String> params = new HashMap<>();
         params.put("key1", "value1");
         String accessToken = request.getHeader(HttpHeaders.AUTHORIZATION);
         params.put(HttpHeaders.AUTHORIZATION, accessToken);
-        return params;
+        if(!exception) {
+            return ResponseEntity.ok(params);
+        } else {
+            return ResponseEntity.badRequest().body(params);
+        }
     }
 
     @PostMapping("post")
