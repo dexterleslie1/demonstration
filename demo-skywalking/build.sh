dfef0d7840f64cd5cad5fb7477949fb36f7e2a36
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-06-28 10:13:20 +0800

    demo-skywalking使用docker测试部署

--- demo-skywalking/build.sh
+++ demo-skywalking/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # 编译elasticsearch
-docker build --tag docker.118899.net:10001/yyd-public/demo-skywalking-elasticsearch --file Dockerfile-elasticsearch .
+#docker build --tag docker.118899.net:10001/yyd-public/demo-skywalking-elasticsearch --file Dockerfile-elasticsearch .
+
+mvn package
