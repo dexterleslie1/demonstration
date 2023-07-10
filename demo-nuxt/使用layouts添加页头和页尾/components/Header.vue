d215da984b6af1e7ce4d5ef59fbff7c09c5fcffd
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-07-10 20:45:18 +0800

    demo-nuxt新增使用layouts添加页头和页尾

---
+++ demo-nuxt/使用layouts添加页头和页尾/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <nuxt-link to="/home">首页</nuxt-link>
+        <nuxt-link to="/about">关于</nuxt-link>
+    </div>
+</template>
---
+++ demo-nuxt/使用layouts添加页头和页尾/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <Header/>
+
+        <!-- nuxt-link跳转显示区域，相当于router-view -->
+        <nuxt/>
+
+        <Footer/>
+    </div>
+</template>
