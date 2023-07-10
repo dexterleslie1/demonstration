6540927924cab79b84416f0fb70094b674ac77a0
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-07-10 21:45:25 +0800

    demo-nuxt新增参数校验和统一错误页面

---
+++ demo-nuxt/参数校验和统一错误页面/layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h6>统一错误界面</h6>
+        <div>{{ error.statusCode }} - {{ error.message }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['error']
+}
+</script>
---
+++ demo-nuxt/参数校验和统一错误页面/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nuxt-link to="/validateParams?p1=111">参数校验</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
