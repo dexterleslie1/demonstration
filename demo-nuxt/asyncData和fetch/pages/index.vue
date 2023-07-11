84bcc9f9a3a3c19f043e5c6c93d5e986e20ff76d
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-07-11 22:25:27 +0800

    demo-nuxt新增asyncData和fetch

---
+++ demo-nuxt/asyncData和fetch/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  // 运行在服务器端axios
+  async asyncData({$axios}) {
+    await $axios.get('/api/v1/test')
+    .then(function(response) {
+      console.log(`my response=${JSON.stringify(response.data)}`)
+    }).catch(function(error) {
+      console.log(`my error=${error}`)
+    })
+  },
+  // 运行在服务器端axios
+  async fetch({$axios}) {
+    await $axios.get('/api/v1/test')
+    .then(function(response) {
+      console.log(`fetch my response=${JSON.stringify(response.data)}`)
+    }).catch(function(error) {
+      console.log(`fetch my error=${error}`)
+    })
+  },
+
+  name: 'IndexPage'
+}
+</script>
