c1acbad4c566cec5e86a494d126a583ce2edbbb1
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-07-12 14:46:18 +0800

    demo-nuxt添加element-ui安装与配置

---
+++ demo-nuxt/element-ui安装与配置/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-button type="primary" @click="handleClick">点击我</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  
+  methods: {
+    handleClick() {
+      alert('Hello world!')
+    }
+  }
+}
+</script>
