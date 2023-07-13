17af4528929d7cff83b05056c41d2f5d87cc4479
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-07-13 15:30:15 +0800

    demo-nuxt新增img动态切换图片demo

---
+++ demo-nuxt/img动态切换图片/pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <hr>
+    <div>
+      演示动态切换img图片<br>
+      https://graffino.com/til/jN9xnHChCC-how-to-add-dynamic-images-in-nuxtjs-vuejs</div>
+    <div>
+      <!-- <img src="~/assets/images/chevron-down.svg" style="width:30px;height:30px;"/> -->
+      <img :src="require(`~/assets/images/${imgName}.svg`)" style="width:30px;height:30px;" />
+      <img :src="imgSrc" style="width:30px;height:30px;" />
+    </div>
+    <div>
+      <button @click="handleClick">点击我切换图片</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imgName: 'chevron-down'
+    }
+  },
+  computed: {
+    imgSrc() {
+      return require(`~/assets/images/${this.imgName}.svg`)
+    }
+  },
+  name: 'IndexPage',
+  methods: {
+    handleClick() {
+      if (this.imgName === 'chevron-down')
+        this.imgName = 'chevron-up'
+      else
+        this.imgName = 'chevron-down'
+    }
+  }
+}
+</script>
