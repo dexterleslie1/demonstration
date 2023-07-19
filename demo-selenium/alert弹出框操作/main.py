d2489c7c3ce0bca07b443d6ec3f0a7410145469a
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-07-19 10:10:34 +0800

    demo-selenium新增alert弹出框操作

---
+++ demo-selenium/alert弹出框操作/main.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+if __name__ == "__main__":
+    try:
+        driver = webdriver.Firefox()
+        driver.get("http://127.0.0.1:5500/web-auxiliary-tool-for-testing/index.html")
+
+        element = driver.find_element(By.CLASS_NAME, 'box3')
+
+        # https://www.browserstack.com/guide/action-class-selenium-python
+        action = ActionChains(driver)
+        action.move_to_element(element)
+        action.click().perform()
+
+        # 等待alert框
+        # https://stackoverflow.com/questions/11467471/how-to-check-if-an-alert-exists-using-webdriver
+        wait = WebDriverWait(driver, 1)
+        wait.until(expected_conditions.alert_is_present())
+
+        # 断言弹出框内容
+        # https://stackoverflow.com/questions/46101336/webdriver-switchto-alert-function-fails
+        # https://stackoverflow.com/questions/47074558/how-to-read-the-text-from-the-alert-box-using-python-selenium
+        alert = driver.switch_to.alert
+        alert_text = alert.text
+        assert alert_text == 'x=8,y=58,width=50,height=50'
+        alert.accept()
+
+    finally:
+        driver.quit()
+pass
