a8eb4f300422c3f2c84c8bc3d385fd54d907e0a7
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-07-19 17:11:03 +0800

    更新python-basic

---
+++ python/python-basic/demo-lib-math.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 演示math库使用
+
+# region 演示math.ceil和math.floor使用
+import math
+
+var_double = 5.56
+
+assert 5 == math.floor(var_double)
+assert 6 == math.ceil(var_double)
+
+# endregion
--- python/python-basic/string+manipulation/main.py
+++ python/python-basic/string+manipulation/main.py
@@ -8,3 +8,14 @@ if var_string.lower() == "y":
     print("Yes")
 else:
     print("No")
+
+# region 字符串是否包含
+
+# https://www.php.cn/faq/420013.html
+
+var_string = '测试字符串1'
+assert '字符' in var_string
+
+assert var_string.find('字符') != -1
+
+# endregion
