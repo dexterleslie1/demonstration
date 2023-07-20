112636b90f277bca92528d34ab6dc3ee0e157429
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-07-20 11:15:01 +0800

    front-end添加demo-vue2-splitpanes演示使用第三方插件splitpanes

---
+++ front-end/综合案例/demo-vue2-splitpanes/README.md
@@ -0,0 +1,26 @@
+# demo-vue2-splitpanes
+
+> 学习使用第三方插件splitpanes
+> https://antoniandre.github.io/splitpanes/?ref=madewithvuejs.com
+>
+> NOTE: 有一个坑，<splitpanes style="height: 400px" class="default-theme">，！！要添加class="default-theme"
+
+
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ front-end/综合案例/demo-vue2-splitpanes/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <!-- <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div> -->
+
+  <splitpanes style="height: 400px" class="default-theme">
+    <pane min-size="20">1</pane>
+    <pane>
+      <splitpanes horizontal>
+        <pane>2</pane>
+        <pane>3</pane>
+        <pane>4</pane>
+      </splitpanes>
+    </pane>
+    <pane>5</pane>
+  </splitpanes>
+</template>
+
+<script>
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
+
+export default {
+  components: { Splitpanes, Pane },
+}
+
+</script>
+
+<style>
+.splitpanes__pane {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* font-family: Helvetica, Arial, sans-serif; */
+  /* color: rgba(255, 255, 255, 0.6); */
+  /* font-size: 5em; */
+}
+
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+} */
+</style>
