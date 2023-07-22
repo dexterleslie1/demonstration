b205bfc5f3b1a0ea3c7528bc1a3af6e6bbf81f45
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-07-22 20:37:52 +0800

    demo-shell-scripting新增演示检查文件是否存在

---
+++ demo-shell-scripting/demo-check-if-file-exists.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 演示判断文件是否存在
+# https://stackoverflow.com/questions/40082346/how-to-check-if-a-file-exists-in-a-shell-script
+if [ -e test.txt ] 
+then
+    echo 'file exists'
+else
+    echo 'file not exists'
+fi
