c318a365723e70c27ef3a7935617b0bbc1ee9dd4
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-07-27 14:49:52 +0800

    demo-nuxt添加自定义组件

---
+++ demo-nuxt/自定义组件/components/sub/Component1.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <ComponentInclude1></ComponentInclude1>
+        <div>组件1</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Component1',
+}
+</script>
---
+++ demo-nuxt/自定义组件/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <NuxtLink to="/">首页</NuxtLink>
+        <NuxtLink to="/component1">组件1</NuxtLink>
+        <NuxtLink to="/component2">组件2</NuxtLink>
+        <NuxtLink to="/component3">组件3</NuxtLink>
+
+        <nuxt />
+    </div>
+</template>
