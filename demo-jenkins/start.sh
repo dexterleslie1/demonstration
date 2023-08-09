b7937b3b78230fbfaf66a65951e3b57d87cc9177
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-08-09 11:18:11 +0800

    优化demo-jenkins

--- demo-jenkins/start.sh
+++ demo-jenkins/start.sh
@@ -11,7 +11,12 @@ do
     sleep 5
 done
 
+# 因为创建仓库时initialize_with_readme，偶尔克隆仓库后版本落后于服务器版本，所以休眠一点时间等待initialize_with_readme完毕
+sleep 5
+
 cp Jenkinsfile test-temp
 (cd test-temp && git add .) || exit $?
 (cd test-temp && git commit -m '初始化') || exit $?
 (cd test-temp && git push) || exit $?
+
+rm -rf test-temp 
