fa13a34c73b52006bf1d22c073f6821dc205e491
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-08-14 11:50:07 +0800

    demo-shell-scripting子shell执行失败退出改进

--- demo-shell-scripting/demo-exit-when-subshell-failed.sh
+++ demo-shell-scripting/demo-exit-when-subshell-failed.sh
@@ -3,6 +3,6 @@
 # 当subshell错误时终止shell执行
 # https://stackoverflow.com/questions/14970663/why-doesnt-bash-flag-e-exit-when-a-subshell-fails
 
-(commandnotexists) || exit $?
+(commandnotexists) || { echo '命令执行失败'; exit 1; }
 
 echo "Hello world!"
