b8a67ba6a25d7748a03ab4d371a36c5af52f1d68
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-08-28 10:51:21 +0800

    spring-boot-mybatis-plus保存枚举int到数据库

--- spring-boot-mybatis-plus/README.md
+++ spring-boot-mybatis-plus/README.md
@@ -24,3 +24,14 @@ docker-compose up -d
 - [mybatis-plus.configuration.map-underscore-to-camel-case开启下划线转化为驼峰](https://blog.csdn.net/weixin_43314519/article/details/109351688)
 - [mybatis-plus执行自定义SQL](https://cloud.tencent.com/developer/article/1531517)
 - [指定entity id auto_increment类型](https://blog.csdn.net/lzhcoder/article/details/112860695)
+
+
+
+## 使用枚举int保存到数据库
+
+> https://blog.csdn.net/qq_50652600/article/details/126135351
+>
+> 配置步骤:
+>
+> - 在model对应的枚举字段配置注解@EnumValue
+> - 在application.properties配置mybatis-plus.type-enums-package=com.future.demo@EnumValue注解包扫描路径
---
+++ spring-boot-mybatis-plus/src/main/java/com/future/demo/mybatis/plus/entity/OperationLogModel.java
@@ -0,0 +1,44 @@
+package com.future.demo.mybatis.plus.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ *
+ * 操作日志
+ */
+@Data
+@TableName(value = "operation_log", autoResultMap = true)
+public class OperationLogModel {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    // 当前操作日志属于哪个用户，操作日志列表接口根据该字段获取指定用户下的操作日志
+    @TableField("auth_id")
+    private Long authId;
+
+    // 操作人
+    @TableField("operator_id")
+    private Long operatorId;
+
+    // 被操作人
+    @TableField("passive_id")
+    private Long passiveId;
+
+    // 操作类型
+    @TableField("operation_type")
+    @EnumValue
+    private OperationType operationType;
+
+    // 操作内容
+    @TableField("content")
+    private String content;
+
+    // 操作时间
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
+    @TableField("create_time")
+    private Date createTime;
+}
