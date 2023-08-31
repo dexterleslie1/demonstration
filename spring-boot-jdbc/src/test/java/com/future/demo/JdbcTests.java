91e4bdbe56f74a61571772d26d93ed56230dc265
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-08-31 16:30:47 +0800

    可忽略提交

--- spring-boot-jdbc/src/test/java/com/future/demo/JdbcTests.java
+++ spring-boot-jdbc/src/test/java/com/future/demo/JdbcTests.java
@@ -1,134 +1,77 @@
 package com.future.demo;
 
-import com.yyd.common.exception.BusinessException;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.sql.*;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-// https://blog.csdn.net/a23452/article/details/126680840
+// https://mariadb.com/ko/resources/blog/how-to-benchmark-mariadb-mysql-using-java-connector/
+@State(Scope.Benchmark)
+@Warmup(iterations = 5, timeUnit = TimeUnit.SECONDS, time = 1)
+@Measurement(iterations = 5, timeUnit = TimeUnit.SECONDS, time = 15)
+@Fork(value = 1, jvmArgs = {"-Xmx2G", "-server"})
+@Threads(value = -1) // detecting CPU count
 @BenchmarkMode(Mode.Throughput)
-@State(Scope.Benchmark) //使用的SpringBoot容器，都是无状态单例Bean，无安全问题，可以直接使用基准作用域BenchMark
 @OutputTimeUnit(TimeUnit.SECONDS)
-//@Fork(1)  //整体平均执行1次
-@Warmup(iterations = 4, time = 5, timeUnit = TimeUnit.SECONDS) //预热1s
-@Measurement(iterations = 5, time = 30, timeUnit = TimeUnit.SECONDS) //测试也是1s、五遍
-// 指定并发执行线程数
-// https://stackoverflow.com/questions/39644383/jmh-run-benchmark-concurrently
-@Threads(-1)
 public class JdbcTests {
 
-    Random random = new Random();
-    OperationLogService operationLogService;
-    JdbcTemplate jdbcTemplate;
+    @State(Scope.Thread)
+    public static class MyState {
 
-    List<Long> userIdList = new ArrayList<>();
+        private Connection connection;
 
-    //springBoot容器
-    private ApplicationContext context;
+//        @Param({"1", "10", "100", "1000", "10000"})
+//        public int size;
+
+        @Param({"false", "true"})
+        public String binary;
+
+
+        @Setup(Level.Trial)
+        public void createConnections() throws Exception {
+            String connectionString = String.format(
+                    "jdbc:mariadb://192.168.1.193:50000/demo_db?user=root&password=123456&useServerPrepStmts=%s",
+                    binary);
+            connection = DriverManager.getConnection(connectionString);
+        }
+
+        @TearDown(Level.Trial)
+        public void doTearDown() throws SQLException {
+            connection.close();
+        }
+    }
+
+    @Benchmark
+    public Date[] testSeq(MyState state) throws SQLException {
+        int i = 0;
+//        int[] values = new int[state.size];
+//        int values[] = new int[1];
+        Date values[] = new Date[1];
+
+//        try (PreparedStatement prep = state.connection.prepareStatement("select * from seq_1_to_" + state.size)) {
+        try (PreparedStatement prep = state.connection.prepareStatement("select now()")) {
+            ResultSet rs = prep.executeQuery();
+            while (rs.next()) {
+//                values[i++] = rs.getInt(1);
+                values[i++] = rs.getDate(1);
+            }
+        }
+
+        return values;
+    }
 
     public static void main(String[] args) throws RunnerException {
         //使用注解之后只需要配置一下include即可，fork和warmup、measurement都是注解
         Options opt = new OptionsBuilder()
                 .include(JdbcTests.class.getSimpleName())
-                // 断点调试时fork=0
-                .forks(1)
                 // 发生错误停止测试
                 .shouldFailOnError(true)
-                .jvmArgs("-Xmx2G", "-server")
                 .build();
         new Runner(opt).run();
     }
-
-    /**
-     * 初始化，获取springBoot容器，run即可，同时得到相关的测试对象
-     */
-    @Setup
-    public void setup() {
-        //容器获取
-        context = SpringApplication.run(Application.class);
-        //获取对象
-        this.operationLogService = context.getBean(OperationLogService.class);
-        this.jdbcTemplate = context.getBean(JdbcTemplate.class);
-
-        for (long i = 1; i <= 200000; i++) {
-            userIdList.add(i);
-        }
-    }
-
-    /**
-     * 测试的后处理操作，关闭容器，资源清理
-     */
-    @TearDown
-    public void teardown() {
-        //使用子类ConfigurableApplicationContext关闭
-        ((ConfigurableApplicationContext) context).close();
-    }
-
-    //    @Benchmark
-    public void testAdd() {
-        int randomIndex = random.nextInt(userIdList.size());
-        Long userId = userIdList.get(randomIndex);
-        Long operatorId = userId;
-        Long passiveId = userId;
-        randomIndex = random.nextInt(OperationType.values().length);
-        OperationType operationType = OperationType.values()[randomIndex];
-        int randomLength = random.nextInt(2048);
-        String content = RandomStringUtils.randomAlphanumeric(randomLength);
-        this.operationLogService.add(userId, operatorId, passiveId, operationType, content);
-    }
-
-    @Benchmark
-    public void testList() throws BusinessException {
-        int randomIndex = random.nextInt(userIdList.size());
-        Long userId = userIdList.get(randomIndex);
-        int randomPage = random.nextInt(100);
-        int randomSize = random.nextInt(100);
-        if (randomSize <= 0)
-            randomSize = 1;
-
-        List<OperationType> operationTypeList = this.getRandomOperationTypeList();
-        this.operationLogService.list(userId, operationTypeList, randomPage, randomSize);
-    }
-
-    //    @Benchmark
-    public void testConnectAndDisconnect() throws SQLException {
-        Connection connection = null;
-        try {
-            connection = this.jdbcTemplate.getDataSource().getConnection();
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
-        }
-
-    }
-
-    List<OperationType> getRandomOperationTypeList() {
-        List<OperationType> operationTypeList = null;
-        int randomLength = random.nextInt(OperationType.values().length);
-        if (randomLength > 0) {
-            operationTypeList = new ArrayList<>();
-            for (int i = 0; i < randomLength; i++) {
-                int randomIndex = random.nextInt(OperationType.values().length);
-                OperationType operationType = OperationType.values()[randomIndex];
-                operationTypeList.add(operationType);
-            }
-        }
-        return operationTypeList;
-    }
-
 }
