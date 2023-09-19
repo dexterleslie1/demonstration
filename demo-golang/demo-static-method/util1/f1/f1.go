60655d84cfc94d90f0aa9fd0e72f3e0ac6263339
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-09-19 14:28:01 +0800

    可忽略提交

--- demo-golang/demo-static-method/util1/f1/f1.go
+++ demo-golang/demo-static-method/util1/f1/f1.go
@@ -2,6 +2,10 @@ package util1
 
 import "fmt"
 
+// Comment paragraph 1
+//
+// Comment paragraph 2
 func MethodF1() {
+	// Comment line xxx
 	fmt.Println("Method util1/f1")
 }
--- demo-golang/demo-static-method/util2/f1/f1.go
+++ demo-golang/demo-static-method/util2/f1/f1.go
@@ -2,6 +2,10 @@ package util2
 
 import "fmt"
 
+// Comment paragraph 1
+//
+// Comment paragraph 2
 func MethodF1() {
+	// Comment line xxx
 	fmt.Println("Method util2/f1")
 }
