4a2bfc709a2248cfed3e33bfca502afea1ff33db
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-09-20 17:21:07 +0800

    demo-golang添加演示string和[]byte相互转换

--- demo-golang/demo-type-string.go
+++ demo-golang/demo-type-string.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
 	// 测试string数据类型
@@ -19,5 +22,15 @@ func main() {
 		fmt.Println("myString1字符串为空")
 	} else {
 		fmt.Println("myString1字符串不为空")
+	}
+
+	// string和byte[]互相转换
+	// https://stackoverflow.com/questions/40632802/how-to-convert-byte-array-to-string-in-go
+	// https://www.tutorialspoint.com/golang-program-to-convert-string-value-to-byte-value
+	str := "我爱中国，Hello world!"
+	byteArr := []byte(str)
+	resultStr := string(byteArr)
+	if str != resultStr {
+		log.Fatal("非预期值")
 	}
 }
