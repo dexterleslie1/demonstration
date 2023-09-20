9c8fd6bc8a3c508836a9bd1c0241b835fb7e299f
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-09-20 16:42:19 +0800

    demo-golang新增http lib演示

--- demo-golang/demo-http-lib/demo_http_lib_test.go
+++ demo-golang/demo-http-lib/demo_http_lib_test.go
@@ -8,9 +8,11 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"testing"
 )
 
-func TestDownfile() {
+// 测试下载文件
+func TestDownloadFile(t *testing.T) {
 	// https://golangdocs.com/golang-download-files
 
 	// 使用net/http库下载文件
@@ -47,10 +49,14 @@ func TestDownfile() {
 
 	defer file.Close()
 
-	fmt.Printf("Downloaded a file %s with size %d", fileName, size)
+	resultStr := fmt.Sprintf("Downloaded a file %s with size %d", fileName, size)
+	if "Downloaded a file kubectl-v1.26.0 with size 309" != resultStr {
+		t.Fatalf("没有预期值")
+	}
 }
 
-func TestGetResponseHeaderValue() (string, error) {
+// 测试获取http响应头
+func TestGetResponseHeaderValue(t *testing.T) {
 	url := "https://bucketxyh.oss-cn-hongkong.aliyuncs.com/kubernetes/kubectl-v1.26.0-linux"
 	client := http.Client{
 		CheckRedirect: func(r *http.Request, via []*http.Request) error {
@@ -61,18 +67,44 @@ func TestGetResponseHeaderValue() (string, error) {
 
 	resp, err := client.Head(url)
 	if err != nil {
-		// log.Fatal(err)
-		return "", err
+		t.Fatalf("%s", err)
 	}
 	defer resp.Body.Close()
 
-	return resp.Header.Get("ETag"), nil
+	resultStr := resp.Header.Get("ETag")
+	if "\"FE8CA6F9EF6DEC468DBFD0A6E1C9D63C\"" != resultStr {
+		t.Fatalf("没有预期值")
+	}
 }
 
-func main() {
-	headerValue, err := TestGetResponseHeaderValue()
+// 演示http下载文件
+// https://stackoverflow.com/questions/11692860/how-can-i-efficiently-download-a-large-file-using-go
+func TestDownloadLargeFile(t *testing.T) {
+	// Create the file
+	var filepath = "/tmp/1.txt"
+	out, err := os.Create(filepath)
 	if err != nil {
-		panic(err)
+		t.Fatalf("%s", err)
 	}
-	fmt.Println("eTag=", headerValue)
+	defer out.Close()
+
+	// Get the data
+	var url = "https://www.baidu.com"
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	defer resp.Body.Close()
+
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		err = fmt.Errorf("bad status: %s", resp.Status)
+		t.Fatalf("%s", err)
+	}
+
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
 }
