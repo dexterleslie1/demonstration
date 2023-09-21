627afee445210c1425ef798fffc86ca1bc2f2009
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-09-21 20:42:33 +0800

    demo-golang演示unit test init函数用法

--- demo-golang/unittest/add_test.go
+++ demo-golang/unittest/add_test.go
@@ -1,6 +1,15 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+// 在测试执行前先执行init函数
+// https://stackoverflow.com/questions/23729790/how-can-i-do-test-setup-using-the-testing-package-in-go
+func init() {
+	fmt.Println("在执行测试前先加载")
+}
 
 func TestAdd(t *testing.T) {
 	result := Add(1, 2)
