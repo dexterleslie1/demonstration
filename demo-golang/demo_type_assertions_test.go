e954d2d698e68759e9b17a297aed7c0c52aafc47
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-09-27 17:12:10 +0800

    demo-golang新增type assertions演示

---
+++ demo-golang/demo_type_assertions_test.go
@@ -0,0 +1,25 @@
+// 演示type assertions用法
+// https://go.dev/tour/methods/15
+
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTypeAssertions(t *testing.T) {
+	var i interface{} = "hello"
+
+	s := i.(string)
+	fmt.Println(s)
+
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	// f = i.(float64) // panic
+	// fmt.Println(f)
+}
