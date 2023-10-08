ab1c900629fc584d0f4daec1a92f5d6de359570c
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-10-08 17:41:37 +0800

    新增demo-make

---
+++ demo-c++/demo-make/demo-shared-library/demo-executable/include/mylib.h
@@ -0,0 +1,14 @@
+/*************************************************************************
+#	> File Name:mylib.h
+#	> Author: dexterleslie
+#	> Mail: dexterleslie@gmail.com
+#	> Created Time: 日 11/13 00:42:12 2022
+ ************************************************************************/
+
+#ifndef _MYLIB_H_
+#define _MYLIB_H_
+
+int add(int a, int b);
+int sub(int a, int b);
+
+#endif
