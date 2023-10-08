25b3353b49537271c44fdba23b30a08d946619a9
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-10-09 07:17:48 +0800

    新增demo-java-utils

---
+++ demo-java-utils/src/test/java/com/future/demo/util/ThreadLocalRandomTests.java
@@ -0,0 +1,33 @@
+package com.future.demo.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ThreadLocalRandomTests {
+    @Test
+    public void test() {
+        int minimum = 0;
+        int maximum = 10;
+        List<Integer> allList = new ArrayList<>();
+        for(int i=minimum; i<maximum; i++) {
+            allList.add(i);
+        }
+
+        List<Integer> randomGenList = new ArrayList<>();
+        for(int i=0; i<1000; i++) {
+            int randomInt = ThreadLocalRandom.current().nextInt(minimum, maximum);
+            if(!randomGenList.contains(randomInt)) {
+                randomGenList.add(randomInt);
+            }
+        }
+
+        Collections.sort(allList);
+        Collections.sort(randomGenList);
+        Assert.assertArrayEquals(randomGenList.toArray(), allList.toArray());
+    }
+}
