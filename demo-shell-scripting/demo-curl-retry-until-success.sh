1632f7bab08dc54bffe8dddadfc680ed532e3b17
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-10-22 22:04:14 +0800

    可忽略提交

--- demo-shell-scripting/demo-curl-retry-until-success.sh
+++ demo-shell-scripting/demo-curl-retry-until-success.sh
@@ -1,12 +1,13 @@
+# https://unix.stackexchange.com/questions/644343/bash-while-loop-stop-after-a-successful-curl-request
 #!/bin/bash
 
 # target_domain=www.baidu.com
 target_domain=x79878899.com
 
-while ! curl -s -f -o /dev/null $target_domain;
+while ! curl -s -f -o /dev/null --connect-timeout 5 $target_domain;
 do
- echo "`date` - 命令curl -s -f -o /dev/null $target_domain 执行失败重试"
+ echo "`date` - 命令curl -s -f -o /dev/null --connect-timeout 5 $target_domain 执行失败重试"
  sleep 1
 done
 
-echo "`date` - 命令curl -s -f -o /dev/null $target_domain 成功执行"
+echo "`date` - 命令curl -s -f -o /dev/null --connect-timeout 5 $target_domain 成功执行"
