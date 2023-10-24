2395a504b38c49206cef848c2d35e4ea333f3616
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-10-24 19:53:59 +0800

    demo-golang改进demo-os-exec.go演示

--- demo-golang/demo-os-exec.go
+++ demo-golang/demo-os-exec.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os/exec"
 )
@@ -21,5 +23,33 @@ func main() {
 		log.Fatal(err, " ", string(out))
 	} else {
 		fmt.Println("date命令执行结果:", string(out))
+	}
+
+	// 显示进度的exec
+	// https://stackoverflow.com/questions/65625672/get-output-of-os-exec-to-to-show-progress-executed-command
+	cmd := exec.Command("./demo-os-exec-scripts.sh")
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal("获取stdout pipe错误，原因: ", err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal("获取stderr pipe错误，原因: ", err)
+	}
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal("调用cmd.Start()错误，原因: ", err)
+	}
+
+	scanner := bufio.NewScanner(io.MultiReader(stdout, stderr))
+	// scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		m := scanner.Text()
+		fmt.Println(m)
+	}
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatal("调用cmd.Wait()错误，原因: ", err)
 	}
 }
