483acb42894e85889dc8854c5a34a8de36628c3c
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-11-16 10:51:42 +0800

    demo-maven新增演示使用命令mvn运行unittest

--- demo-maven/README.md
+++ demo-maven/README.md
@@ -8,3 +8,27 @@
 mvn -pl ../xxx-module -am package
 ```
 
+
+
+## 使用mvn命令运行unittest
+
+> https://stackoverflow.com/questions/75939658/how-to-run-tests-using-maven
+
+```shell
+# 在pom.xml中添加
+<build>
+    <pluginManagement>
+      <plugins>
+        <plugin>
+          <groupId>org.apache.maven.plugins</groupId>
+          <artifactId>maven-surefire-plugin</artifactId>
+          <version>3.0.0</version>
+        </plugin>
+      </plugins>
+    </pluginManagement>
+  </build>
+ 
+# 运行测试
+mvn test
+```
+
---
+++ demo-maven/demo-unittest/src/test/java/com/future/demo/Tests.java
@@ -0,0 +1,23 @@
+package com.future.demo;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+public class Tests {
+    @Test
+    public void test() {
+        Assert.assertEquals(1, 1);
+    }
+}
