1305890de8a3d7b5a12e8b38598af9814b874a1f
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-11-18 18:44:44 +0800

    demo-lib-promptui新增demo-select

--- demo-golang/demo-lib-promptui/demo-select/main.go
+++ demo-golang/demo-lib-promptui/demo-select/main.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"fmt"
-	"testing"
 
 	"github.com/manifoldco/promptui"
 )
 
-func TestSelect(t *testing.T) {
+func main() {
 	prompt := promptui.Select{
 		Label: "Select Day",
 		Items: []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
 			"Saturday", "Sunday"},
 	}
 
-	_, result, err := prompt.Run()
+	index, result, err := prompt.Run()
 
 	if err != nil {
 		fmt.Printf("Prompt failed %v\n", err)
 		return
 	}
 
-	fmt.Printf("You choose %q\n", result)
+	fmt.Printf("You choose %d %q\n", index, result)
 }
