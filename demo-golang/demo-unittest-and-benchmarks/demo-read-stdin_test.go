5cd502f293c9d819f976dd43705cd01e85ef32eb
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-11-20 10:20:13 +0800

    可忽略提交

--- demo-golang/demo-unittest-and-benchmarks/demo-read-stdin_test.go
+++ demo-golang/demo-unittest-and-benchmarks/demo-read-stdin_test.go
@@ -3,20 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
 	"strings"
 	"testing"
 )
-
-// 单元测试读取os.Stdin总是返回EOF
-func TestReadStdin(t *testing.T) {
-	buf := bufio.NewReader(os.Stdin)
-	r, _, err := buf.ReadRune()
-	if err != nil {
-		t.Errorf("expected no err, got %s", err)
-	}
-	fmt.Println("你的输入：", string(r))
-}
 
 // stdin mock
 func TestStdinMock(t *testing.T) {
