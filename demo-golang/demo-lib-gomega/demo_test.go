8f79a7b9d15b632ae2e7d7618d8a3927a0c74c9b
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-12-28 22:42:29 +0800

    demo-lib-gomega添加error expect相关用法演示

--- demo-golang/demo-lib-gomega/demo_test.go
+++ demo-golang/demo-lib-gomega/demo_test.go
@@ -1,6 +1,7 @@
 package demo_lib_gomega
 
 import (
+	"errors"
 	"github.com/onsi/gomega"
 	"testing"
 )
@@ -11,4 +12,16 @@ func TestDemo(t *testing.T) {
 	g := gomega.NewWithT(t)
 	b := true
 	g.Expect(b).To(gomega.BeTrue())
+
+	// error测试
+	g.Expect(myFun1()).To(gomega.Succeed())
+	g.Expect(myFun1()).NotTo(gomega.HaveOccurred())
+	g.Expect(myFun2()).ToNot(gomega.Succeed())
+}
+
+func myFun1() error {
+	return nil
+}
+func myFun2() error {
+	return errors.New("测试错误")
 }
