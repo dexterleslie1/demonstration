0156b139fcfaec403c14c3532f6633c44748a388
dexterleslie@gmail.com <dexterleslie@gmail.com>
2024-01-04 18:41:22 +0800

    demo-controller-runtime添加查询replicaset相关的pod列表演示

--- demo-kubernetes/demo-client/demo-test/demo-controller-runtime/demo-controller-runtime-builtin-resource_test.go
+++ demo-kubernetes/demo-client/demo-test/demo-controller-runtime/demo-controller-runtime-builtin-resource_test.go
@@ -2,7 +2,9 @@ package demo_controller_runtime
 
 import (
 	"context"
+	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/util/retry"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
@@ -87,5 +89,63 @@ func TestControllerRuntimeBuiltinResource(t *testing.T) {
 	}
 	if !found {
 		t.Fatalf("expected true, got %t", found)
+	}
+}
+
+func TestControllerRuntimeReplicaSetAndPod(t *testing.T) {
+	cl, err := client.New(config.GetConfigOrDie(), client.Options{})
+	if err != nil {
+		t.Fatalf("expected no err, got %s", err)
+	}
+
+	replicaSetName := "replicaset1"
+
+	// 创建 replicaset
+	replicaSet := &v1.ReplicaSet{}
+	replicaSet.SetNamespace(corev1.NamespaceDefault)
+	replicaSet.SetName(replicaSetName)
+	replicas := int32(2)
+	replicaSet.Spec.Replicas = &replicas
+	replicaSet.Spec.Selector = &v12.LabelSelector{
+		MatchLabels: map[string]string{
+			"app": "nginx-pod",
+		},
+	}
+	replicaSet.Spec.Template = corev1.PodTemplateSpec{
+		ObjectMeta: v12.ObjectMeta{
+			Labels: map[string]string{
+				"app": "nginx-pod",
+			},
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:            "nginx",
+					Image:           "nginx",
+					ImagePullPolicy: corev1.PullIfNotPresent,
+				},
+			},
+		},
+	}
+
+	_ = cl.Delete(context.Background(), replicaSet, &client.DeleteOptions{GracePeriodSeconds: new(int64)})
+
+	err = cl.Create(context.Background(), replicaSet, &client.CreateOptions{})
+	if err != nil {
+		t.Fatalf("expected no err, got %s", err)
+	}
+
+	// 查询 replicaset 相关联的 pod 列表
+	// https://github.com/kubernetes-sigs/controller-runtime/blob/main/pkg/builder/example_test.go
+	podList := corev1.PodList{}
+	err = cl.List(context.Background(), &podList, client.MatchingLabels{
+		"app": "nginx-pod",
+	})
+	if err != nil {
+		t.Fatalf("expected no err, got %s", err)
+	}
+
+	if len(podList.Items) != 2 {
+		t.Fatalf("expected 2, got %d", len(podList.Items))
 	}
 }
