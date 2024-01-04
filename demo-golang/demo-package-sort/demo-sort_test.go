d63fab36879d4f6157cf9ee65cc4947602748aa7
dexterleslie@gmail.com <dexterleslie@gmail.com>
2024-01-04 23:30:46 +0800

    demo-golang新增demo-package-sort演示

---
+++ demo-golang/demo-package-sort/demo-sort_test.go
@@ -0,0 +1,25 @@
+package demo_package_sort
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+// https://pkg.go.dev/sort#example-Slice
+func TestDemoSortSlice(t *testing.T) {
+	people := []struct {
+		Name string
+		Age  int
+	}{
+		{"Gopher", 7},
+		{"Alice", 55},
+		{"Vera", 24},
+		{"Bob", 75},
+	}
+	sort.Slice(people, func(i, j int) bool { return people[i].Name < people[j].Name })
+	fmt.Println("By name:", people)
+
+	sort.Slice(people, func(i, j int) bool { return people[i].Age < people[j].Age })
+	fmt.Println("By age:", people)
+}
