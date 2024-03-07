1de4be1628776bb5aa06b78a14840a5dabb41be6
dexterleslie@gmail.com <dexterleslie@gmail.com>
2024-03-07 11:32:12 +0800

    www.hotjar.com添加feedback

---
+++ front-end/html+js+css/综合例子/www.hotjar.com/index.js
@@ -0,0 +1,23 @@
+let languageSwitchTrigger = document.querySelector("#languageSwitchTrigger")
+languageSwitchTrigger.addEventListener("click", () => {
+    let x = document.querySelector(".ul-lang-switch")
+    if (x.style.display === "none" || !x.style.display) {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+})
+
+// 点击 feedback
+let feedback = document.querySelector(".feedback")
+let feedbackSubmitPanel = document.querySelector(".feedback-submit-panel")
+let feebackDispalyOrigin
+feedback.addEventListener("click", () => {
+    feebackDispalyOrigin = feedback.style.display
+    feedback.style.display = "none"
+    feedbackSubmitPanel.style.display = "block"
+})
+feedbackSubmitPanel.querySelector(".close").addEventListener("click", () => {
+    feedback.style.display = feebackDispalyOrigin
+    feedbackSubmitPanel.style.display = "none"
+})
