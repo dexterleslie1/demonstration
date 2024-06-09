95db8eec6956a8ac8fe6f8b2b42a17a8626ee443
dexterleslie@gmail.com <dexterleslie@gmail.com>
2024-06-09 12:14:53 +0800

    改进spring-boot test演示新增feign错误处理

--- demo-spring-boot/demo-spring-boot-test/src/test/java/com/future/demo/test/config/TestSupportConfiguration.java
+++ demo-spring-boot/demo-spring-boot-test/src/test/java/com/future/demo/test/config/TestSupportConfiguration.java
@@ -1,7 +1,10 @@
 package com.future.demo.test.config;
 
+import com.future.common.feign.CustomizeErrorDecoder;
 import com.future.demo.test.feign.TestSupportDemoFeignClient;
+import feign.codec.ErrorDecoder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,4 +14,13 @@ import org.springframework.context.annotation.Configuration;
         }
 )
 public class TestSupportConfiguration {
+        /**
+         * openfeign支持自动检查并抛出业务异常不需要编写代码判断errorCode是否不等于0
+         *
+         * @return
+         */
+        @Bean
+        ErrorDecoder errorDecoder() {
+                return new CustomizeErrorDecoder();
+        }
 }
