3a85a6fdd43427ff4dac91d8aed2c7e5af099a87
dexterleslie@gmail.com <dexterleslie@gmail.com>
2024-06-09 20:53:39 +0800

    spring-boot jdbc演示迁移到demo-spring-boot中

---
+++ demo-spring-boot/demo-spring-boot-jdbc/init-mysql.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS demo_db CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+USE demo_db;
+
+/** 操作记录表 */
+CREATE TABLE IF NOT EXISTS `operation_log`(
+  id                  BIGINT primary key NOT NULL AUTO_INCREMENT,
+  auth_id             BIGINT NOT NULL COMMENT '当前操作日志属于哪个用户，操作日志列表接口根据该字段获取指定用户下的操作日志',
+  operator_id         BIGINT NOT NULL COMMENT '操作人',
+  passive_id          BIGINT NOT NULL COMMENT '被操作人',
+  /*operation_type      VARCHAR(64) NOT NULL COMMENT '操作类型',*/
+  operation_type      TINYINT NOT NULL COMMENT '操作类型',
+  content             TEXT COMMENT '操作内容',
+  create_time         DATETIME NOT NULL COMMENT '操作时间'/*,
+  primary key(id, auth_id)*/
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- demo-spring-boot/demo-spring-boot-jdbc/src/main/java/com/future/demo/Application.java
+++ demo-spring-boot/demo-spring-boot-jdbc/src/main/java/com/future/demo/Application.java
@@ -9,10 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     /**
-     *
      * @param args
      */
-    public static void main(String []args){
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
