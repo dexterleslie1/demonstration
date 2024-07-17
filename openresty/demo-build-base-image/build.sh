22142249b5b06b214a46b6d8693e0a9b3099fa2a
Dexter.Chen <dexterleslie@gmail.com>
2024-07-17 20:55:16 +0800

    新增编译openresty基础镜像演示

---
+++ openresty/demo-build-base-image/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dockerRegistryPrefix=192.168.235.138/library
+
+set -x
+set -e
+
+sudo rm -f openresty.tar
+
+docker build -f DockerfileOpenrestyCompile --tag openresty-compile-dev .
+
+# 复制编译后的/usr/local/openresty目录到宿主机
+docker rm -f openresty-compile-instance-dev
+docker create -it --name openresty-compile-instance-dev openresty-compile-dev /bin/bash
+docker cp openresty-compile-instance-dev:/usr/local/openresty.tar openresty.tar
+
+# docker build -f Dockerfile --tag $dockerRegistryPrefix/demo-openresty-base-dev .
+docker compose build
+
+echo "成功编译openresty基础镜像，镜像标签为： $dockerRegistryPrefix/demo-openresty-base-dev" && sudo rm -f openresty.tar
+
