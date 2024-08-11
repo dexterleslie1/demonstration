70f1565cd0e53741b7ea79b9b5e02f423775fc20
Dexter.Chen <dexterleslie@gmail.com>
2024-08-11 13:19:30 +0800

    新增spring-boot quartz集成和使用文档

--- demo-spring-boot/demo-spring-boot-quartz/src/main/java/com/future/demo/quartz/Task1.java
+++ demo-spring-boot/demo-spring-boot-quartz/src/main/java/com/future/demo/quartz/Task1.java
@@ -1,10 +1,9 @@
 package com.future.demo.quartz;
 
-import java.util.Date;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * 
@@ -36,32 +35,45 @@ public class Task1 {
         System.out.println("onlyOnceTask " + date);
     }
 
-    /**
-     *
-     */
-    @Scheduled(cron = "*/5 * * * * ?")
+    // 每隔5秒触发一次，0/5 表示从0秒开始
+    @Scheduled(cron = "0/5 * * * * ?")
     public void cronbTask() {
         Date date = new Date();
-        System.out.println("cronbTask " + date);
+        System.out.println("每5秒触发一次：" + date);
     }
 
-    // 每分钟执行一次
-    @Scheduled(cron = "0 * * * * ?")
+    // 每分钟触发一次
+    @Scheduled(cron = "0 0/1 * * * ?")
     public void doCronbEveryMinute() {
         Date date = new Date();
-        System.out.println("doCronbEveryMinute " + date);
+        System.out.println("每分钟触发一次：" + date);
     }
 
-    // 每秒钟执行一次
+    // 每秒触发一次
     @Scheduled(cron = "0/1 * * * * ?")
     public void doCronbEverySecond() {
         Date date = new Date();
-        System.out.println("doCronbEverySecond " + date);
+        System.out.println("每秒触发一次：" + date);
     }
 
-    @Scheduled(cron = "0 0/2 10 * * ?")
+    // 每小时的每分钟的第1秒触发一次
+    @Scheduled(cron = "1 * * * * ?")
+    public void triggerOnceAtFirstSecondEveryMinute() {
+        Date date = new Date();
+        System.out.println("每小时的每分钟的第1秒触发一次：" + date);
+    }
+
+    // 每天13:06:03触发一次
+    @Scheduled(cron = "3 6 13 * * ?")
+    public void triggerTest1() {
+        Date date = new Date();
+        System.out.println("每天13:06:03触发一次：" + date);
+    }
+
+    // 每天13点每2分钟触发
+    @Scheduled(cron = "0 0/2 13 * * ?")
     public void doCronbStartAt10Every2Minute() {
         Date date = new Date();
-        System.out.println("doCronbStartAt10Every2Minute " + date);
+        System.out.println("每天13点每2分钟触发：" + date);
     }
 }
--- publisher/vuepress-docs/docs/.vuepress/config.js
+++ publisher/vuepress-docs/docs/.vuepress/config.js
@@ -331,6 +331,7 @@ module.exports = {
                         'spring-boot的@EnableXxx使用.md',
                         '通过parent或dependencymanagement方式管理spring-boot依赖.md',
                         '横向扩展.md',
+                        'spring-boot-quartz.md',
                     ]
                 }
             ], '/java/': [
