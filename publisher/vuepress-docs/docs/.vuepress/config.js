daff56cb6b54eaedb6eac223558adc452e38197c
Dexter.Chen <dexterleslie@gmail.com>
2024-08-29 14:08:39 +0800

    新增操作系统页面缓存、目录项缓存、索引节点缓存文档

--- publisher/vuepress-docs/docs/.vuepress/config.js
+++ publisher/vuepress-docs/docs/.vuepress/config.js
@@ -218,6 +218,7 @@ module.exports = {
                         'ubuntu-安装googlepinyin输入法.md',
                         '配置基于ubuntu的软件开发环境.md',
                         'ubuntu启用ssh服务.md',
+                        '操作系统页面缓存、目录项缓存、索引节点缓存.md',
                     ]
                 }
             ], '/基准和性能测试/': [
