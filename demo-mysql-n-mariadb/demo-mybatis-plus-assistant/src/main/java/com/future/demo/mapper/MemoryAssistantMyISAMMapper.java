d8693cd4f450b76412744ef428c57cd1e7ea80ae
Dexter.Chen <dexterleslie@gmail.com>
2024-09-22 22:36:06 +0800

    mysql mybatis-plus assistant新增MemoryUsageJmhTests

--- demo-mysql-n-mariadb/demo-mybatis-plus-assistant/src/main/java/com/future/demo/mapper/MemoryAssistantMyISAMMapper.java
+++ demo-mysql-n-mariadb/demo-mybatis-plus-assistant/src/main/java/com/future/demo/mapper/MemoryAssistantMyISAMMapper.java
@@ -11,8 +11,8 @@ public interface MemoryAssistantMyISAMMapper extends MPJBaseMapper<MemoryAssista
     @Select("select id from memory_assistant_myisam")
     List<Long> selectIds();
 
-    //    @Select("select * from memory_assistant_myisam where extraIndexId>=#{startId} and extraIndexId<=#{endId} and randomStr=#{randomStr}")
-    @Select("select * from memory_assistant_myisam where randomStr=#{randomStr}")
+    @Select("select * from memory_assistant_myisam where extraIndexId>=#{startId} and extraIndexId<=#{endId} and randomStr=#{randomStr}")
+//    @Select("select * from memory_assistant_myisam where randomStr=#{randomStr}")
     List<MemoryAssistantMyISAMEntity> testReadBufferSize(@Param("startId") long startId,
                                                          @Param("endId") long endId,
                                                          @Param("randomStr") String randomStr);
