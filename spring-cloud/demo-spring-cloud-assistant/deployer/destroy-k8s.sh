c55b32854ac84374ac92d3486b67f606aa925295
Dexter.Chen <dexterleslie@gmail.com>
2024-11-16 21:59:25 +0800

    可忽略提交

--- spring-cloud/demo-spring-cloud-assistant/deployer/destroy-k8s.sh
+++ spring-cloud/demo-spring-cloud-assistant/deployer/destroy-k8s.sh
@@ -2,4 +2,6 @@
 
 kubectl delete -f k8s-application.yaml
 
-kubectl delete pvc `kubectl get pvc|grep redis|awk '{print $1}'`
+kubectl delete pvc `kubectl get pvc|grep redis-cluster|awk '{print $1}'`
+kubectl delete pvc data-elasticsearch-elasticsearch-0
+kubectl delete pvc db-data-db-0
