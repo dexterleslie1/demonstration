cc10bdd644868836d4791f4bbf78b73d32a4d7be
Dexter.Chen <dexterleslie@gmail.com>
2024-12-19 16:06:19 +0800

    SpringBoot redis作为session存储示例迁移到SpringBoot示例目录中

--- demo-spring-boot/demo-spring-boot-session-redis/src/main/java/com/future/demo/ApiController.java
+++ demo-spring-boot/demo-spring-boot-session-redis/src/main/java/com/future/demo/ApiController.java
@@ -1,4 +1,4 @@
-package com.future.study.spring.boot.session.redis;
+package com.future.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,25 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
-/**
- * @author Dexterleslie.Chan
- */
 @RestController
-@RequestMapping(value = "/admin/v1")
 public class ApiController {
-    @RequestMapping(value = "/first", method = RequestMethod.GET)
+    @RequestMapping(value = "/post", method = RequestMethod.GET)
     public Map<String, Object> firstResp (HttpServletRequest request){
         Map<String, Object> map = new HashMap<>();
-        request.getSession().setAttribute("map", request.getRequestURL());
+        request.getSession().setAttribute("datum", request.getRequestURL());
         map.put("request Url", request.getRequestURL());
         return map;
     }
 
-    @RequestMapping(value = "/sessions", method = RequestMethod.GET)
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
     public Object sessions (HttpServletRequest request){
         Map<String, Object> map = new HashMap<>();
         map.put("sessionId", request.getSession().getId());
-        map.put("message", request.getSession().getAttribute("map"));
+        map.put("datum", request.getSession().getAttribute("datum"));
         return map;
     }
 }
