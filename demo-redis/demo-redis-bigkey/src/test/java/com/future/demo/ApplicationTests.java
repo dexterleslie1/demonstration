cd1bdafc4c9f3e16cb11b26a718ec87c3fe322c4
Dexter.Chen <dexterleslie@gmail.com>
2025-01-23 16:38:22 +0800

    Redis BigKey问题新增JMH测试

---
+++ demo-redis/demo-redis-bigkey/src/test/java/com/future/demo/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.future.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {Application.class})
+public class ApplicationTests {
+
+    @Test
+    public void test() {
+    }
+}
