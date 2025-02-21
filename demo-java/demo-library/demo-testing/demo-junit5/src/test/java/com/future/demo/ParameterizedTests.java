6e3ae02446b9f5aa3bcffef39b6b37d115d462c2
Dexter.Chen <dexterleslie@gmail.com>
2025-02-21 10:45:08 +0800

    测试文档新增JUnit5参数化测试使用@MethodSource和@ValueSource作为参数源说明

--- demo-java/demo-library/demo-testing/demo-junit5/src/test/java/com/future/demo/ParameterizedTests.java
+++ demo-java/demo-library/demo-testing/demo-junit5/src/test/java/com/future/demo/ParameterizedTests.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 // 参数化测试
 public class ParameterizedTests {
@@ -37,5 +38,27 @@ public class ParameterizedTests {
     @CsvFileSource(resources = "/test.csv")
     public void testCsvFileSource(int a, int b, int c) {
         Assertions.assertEquals(c, this.demoUtil.add(a, b), a + "+" + b + "=" + c);
+    }
+
+    // 测试 @MethodSource 从另外一个方法中获取参数
+    @ParameterizedTest(name = "加法：{0}+{1}={2}")
+    @MethodSource("methodSourceSupplier")
+    public void testMethodSource(int a, int b, int c) {
+        Assertions.assertEquals(c, this.demoUtil.add(a, b), a + "+" + b + "=" + c);
+    }
+
+    static Stream<Arguments> methodSourceSupplier() {
+        return Stream.of(
+                Arguments.of(1, 2, 3),
+                Arguments.of(4, 5, 9),
+                Arguments.of(7, 8, 15)
+        );
+    }
+
+    // 测试 @ValueSource 从一个数组中获取参数
+    @ParameterizedTest(name = "测试 @ValueSource 从一个数组中获取参数 {0}")
+    @ValueSource(ints = {1, 2, 3})
+    public void testValueSource(int a) {
+        Assertions.assertTrue(a > 0);
     }
 }
