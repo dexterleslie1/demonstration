1ed3388f21cd52bf9d6898e948bc36af015d6123
Dexter.Chen <dexterleslie@gmail.com>
2025-03-08 21:10:32 +0800

    Vue指令文档新增v-model指令说明

---
+++ front-end/demo-vue/demo-v-model/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    使用v-model绑定变量：<input v-model="var1" /><button @click="var1++">+</button>
+    <br />
+    <button @click="handleClick">显示变量值</button>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data() {
+    return {
+      var1: 1,
+    }
+  },
+  methods: {
+    handleClick() {
+      alert(this.var1)
+    }
+  },
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
