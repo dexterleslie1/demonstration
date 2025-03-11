53b1568d9bf3ea130fdcce78c55f4b35d4aef33c
Dexter.Chen <dexterleslie@gmail.com>
2025-03-12 07:51:09 +0800

    Vue指令文档新增v-model input radio、input checkbox、input select说明

--- front-end/demo-vue/demo-v-model/src/App.vue
+++ front-end/demo-vue/demo-v-model/src/App.vue
@@ -5,6 +5,28 @@
     使用v-model绑定变量：<input v-model="var1" /><button @click="var1++">+</button>
     <br />
     <button @click="handleClick">显示变量值</button>
+    <hr />
+
+    <div>v-model input radio 使用</div>
+    <input type="radio" name="radioGroup" value="0" v-model="varRadio" />Radio1
+    <input type="radio" name="radioGroup" value="1" v-model="varRadio" />Radio2
+    <div>varRadio变量值：{{ varRadio }}</div>
+    <hr />
+
+    <div>v-model input checkbox 使用</div>
+    <input type="checkbox" name="checkboxGroup" value="0" v-model="varCheckbox" />Checkbox1
+    <input type="checkbox" name="checkboxGroup" value="1" v-model="varCheckbox" />Checkbox2
+    <div>varCheckbox变量值：{{ varCheckbox }}</div>
+    <hr />
+
+    <div>v-model input select 使用</div>
+    <select name="select" v-model="varSelect" multiple>
+      <option value="0">0</option>
+      <option value="1">1</option>
+    </select>
+    <div>varSelect变量值：{{ varSelect }}</div>
+    <hr />
+
   </div>
 </template>
 
@@ -15,6 +37,14 @@ export default {
   data() {
     return {
       var1: 1,
+      // input radio 变量
+      varRadio: 0,
+      // input checkbox 变量
+      varCheckbox: [1],
+      // 支持多选时 select 变量
+      varSelect: [1],
+      // 单选时 select 变量
+      // varSelect: 0,
     }
   },
   methods: {
