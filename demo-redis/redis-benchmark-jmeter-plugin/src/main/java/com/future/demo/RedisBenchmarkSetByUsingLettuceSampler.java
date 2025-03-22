fd9cc2bcc13e15aadecc46eb9a7b034144ab593e
dexterleslie@gmail.com <dexterleslie@gmail.com>
2025-03-22 10:39:27 +0800

    基于JMeter的Redis基准测试示例新增Lettuce测试

--- demo-redis/redis-benchmark-jmeter-plugin/src/main/java/com/future/demo/RedisBenchmarkSetByUsingLettuceSampler.java
+++ demo-redis/redis-benchmark-jmeter-plugin/src/main/java/com/future/demo/RedisBenchmarkSetByUsingLettuceSampler.java
@@ -1,40 +1,53 @@
 package com.future.demo;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import io.lettuce.core.cluster.RedisClusterClient;
+import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
+import io.lettuce.core.cluster.api.sync.RedisClusterCommands;
 import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 import org.apache.jmeter.samplers.SampleResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import redis.clients.jedis.DefaultJedisClientConfig;
-import redis.clients.jedis.HostAndPort;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisCluster;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * 基于 Jedis 的 set 性能测试
  */
-public class RedisBenchmarkSetByUsingJedisSampler extends AbstractJavaSamplerClient {
-    private static final Logger log = LoggerFactory.getLogger(RedisBenchmarkSetByUsingJedisSampler.class);
+public class RedisBenchmarkSetByUsingLettuceSampler extends AbstractJavaSamplerClient {
+    private static final Logger log = LoggerFactory.getLogger(RedisBenchmarkSetByUsingLettuceSampler.class);
     private static final Random RANDOM = new Random();
 
     /**
      * 是否连接redis集群
      */
     private boolean cluster;
+
     /**
-     * 非redis集群客户端
+     * 集群模式
      */
-    Jedis jedis;
+    RedisClusterClient clusterClient;
     /**
-     * redis集群客户端
+     * 集群模式
      */
-    JedisCluster jedisCluster;
+    StatefulRedisClusterConnection<String, String> clusterConnection;
+    RedisClusterCommands<String, String> clusterSync;
+
+    /**
+     * Standalone模式
+     */
+    RedisClient client;
+    /**
+     * Standalone模式
+     */
+    StatefulRedisConnection<String, String> connection;
+    RedisCommands<String, String> sync;
+
     private final int totalKeyListSize = 100;
     /**
      * 用于测试 set 指令性能的 Key 和 Value
@@ -54,34 +67,40 @@ public class RedisBenchmarkSetByUsingJedisSampler extends AbstractJavaSamplerCli
 
         // 非redis集群
         if (!this.cluster) {
-            if (jedis == null) {
+            if (clusterClient == null) {
                 if (password != null && !password.isEmpty()) {
-                    // 注意：暂时没有找到方法当jedis实例创建失败时停止测试
-                    jedis = new Jedis(new HostAndPort(host, port), DefaultJedisClientConfig.builder().password(password).build());
+                    client = RedisClient.create("redis://" + password + "@" + host + ":" + port);
+                    connection = client.connect();
+                    sync = connection.sync();
 
                     if (log.isDebugEnabled()) {
-                        log.debug("成功创建jedis实例 host {} port {} password *** 线程 {}", host, port, Thread.currentThread().getName());
+                        log.debug("成功创建 Lettuce standalone 实例 host {} port {} password *** 线程 {}", host, port, Thread.currentThread().getName());
                     }
                 } else {
-                    jedis = new Jedis(host, port);
+                    client = RedisClient.create("redis://" + host + ":" + port);
+                    connection = client.connect();
+                    sync = connection.sync();
 
                     if (log.isDebugEnabled()) {
-                        log.debug("成功创建jedis实例 host {} port {} password 无密码模式 线程 {}", host, port, Thread.currentThread().getName());
+                        log.debug("成功创建 Lettuce standalone 实例 host {} port {} password 无密码模式 线程 {}", host, port, Thread.currentThread().getName());
                     }
                 }
             } else {
                 if (log.isDebugEnabled()) {
-                    log.debug("意料之外，jedis实例已经初始化");
+                    log.debug("意料之外，Lettuce standalone 实例已经初始化");
                 }
             }
         } else {
             // redis集群
-            if (this.jedisCluster == null) {
-                this.jedisCluster = new JedisCluster(new HostAndPort(host, port));
-                log.debug("成功创建 Jedis 集群实例，host{} port {} password *** ", host, port);
+            if (this.clusterClient == null) {
+                RedisURI node1 = RedisURI.create(host, port);
+                clusterClient = RedisClusterClient.create(Collections.singletonList(node1));
+                clusterConnection = clusterClient.connect();
+                clusterSync = clusterConnection.sync();
+                log.debug("成功创建 Lettuce 集群实例，host{} port {} password *** ", host, port);
             } else {
                 if (log.isDebugEnabled()) {
-                    log.debug("意料之外，jedisCluster实例已经初始化");
+                    log.debug("意料之外，Lettuce 集群实例已经初始化");
                 }
             }
         }
@@ -90,22 +109,30 @@ public class RedisBenchmarkSetByUsingJedisSampler extends AbstractJavaSamplerCli
     @Override
     public void teardownTest(JavaSamplerContext context) {
         if (!this.cluster) {
-            if (jedis != null) {
-                jedis.close();
-                jedis = null;
+            if (this.connection != null) {
+                this.connection.close();
+                this.connection = null;
+            }
+            if (client != null) {
+                client.shutdown();
+                client = null;
+            }
 
-                if (log.isDebugEnabled()) {
-                    log.debug("成功关闭jedis实例");
-                }
+            if (log.isDebugEnabled()) {
+                log.debug("成功关闭 Lettuce standalone 实例");
             }
         } else {
-            if (this.jedisCluster != null) {
-                this.jedisCluster.close();
-                this.jedisCluster = null;
+            if (clusterConnection != null) {
+                clusterConnection.close();
+                clusterConnection = null;
+            }
+            if (clusterClient != null) {
+                clusterClient.shutdown();
+                clusterClient = null;
+            }
 
-                if (log.isDebugEnabled()) {
-                    log.debug("成功关闭jedisCluster实例");
-                }
+            if (log.isDebugEnabled()) {
+                log.debug("成功关闭 Lettuce 集群实例");
             }
         }
     }
@@ -137,9 +164,9 @@ public class RedisBenchmarkSetByUsingJedisSampler extends AbstractJavaSamplerCli
 
             String keyToWrite = this.keyListToWrite.get(RANDOM.nextInt(totalKeyListSize));
             if (!this.cluster) {
-                this.jedis.set(keyToWrite, keyToWrite);
+                sync.set(keyToWrite, keyToWrite);
             } else {
-                this.jedisCluster.set(keyToWrite, keyToWrite);
+                clusterSync.set(keyToWrite, keyToWrite);
             }
 
             if (log.isDebugEnabled()) {
