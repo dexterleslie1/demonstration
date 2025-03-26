da9b8a6861543d94998821d2ecbeb4eaac5eac4e
dexterleslie@gmail.com <dexterleslie@gmail.com>
2025-03-26 13:59:34 +0800

    Apache Commons Exec文档新增判断命令是否存在说明

--- demo-java/demo-library/apache-commons-exec.md
+++ demo-java/demo-library/apache-commons-exec.md
@@ -1,8 +1,8 @@
 # Apache Commons Exec
 
->`https://blog.csdn.net/u011943534/article/details/120938888`
+>[参考链接](https://blog.csdn.net/u011943534/article/details/120938888)
 
-详细用法请参考示例 `https://gitee.com/dexterleslie/demonstration/tree/master/demo-java/demo-library/demo-commons-exec`
+详细用法请参考本站 [示例](https://gitee.com/dexterleslie/demonstration/tree/master/demo-java/demo-library/demo-commons-exec)
 
 
 
@@ -62,6 +62,37 @@ code = exec.execute(commandLine, new HashMap<String, String>() {{
 System.out.println("退出代码: " + code);
 System.out.println(susStream.toString("GBK"));
 System.out.println(errStream.toString("GBK"));
+
+// endregion
+```
+
+
+
+## 判断命令是否存在
+
+```java
+// region 判断命令是否存在
+
+// 注意：不能直接执行 command 命令，因为它是 sh 内置的命令，所以需要使用 sh 执行此命令
+commandLine = new CommandLine("sh");
+commandLine.addArgument("-c");
+// 使用 command -v 命令判断 libreoffice 命令是否存在
+commandLine.addArgument("command -v libreoffice", false);
+exec = DefaultExecutor.builder().get();
+code = exec.execute(commandLine);
+Assert.assertEquals(0, code);
+
+commandLine = new CommandLine("sh");
+commandLine.addArgument("-c");
+// 使用 command -v 命令判断 libreoffice 命令是否存在
+commandLine.addArgument("command -v libreofficex", false);
+exec = DefaultExecutor.builder().get();
+try {
+    exec.execute(commandLine);
+    Assert.fail();
+} catch (ExecuteException ex) {
+    Assert.assertEquals(127, ex.getExitValue());
+}
 
 // endregion
 ```
--- demo-java/demo-library/demo-commons-exec/src/test/java/com/future/demo/Tests.java
+++ demo-java/demo-library/demo-commons-exec/src/test/java/com/future/demo/Tests.java
@@ -2,12 +2,16 @@ package com.future.demo;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
 import org.apache.commons.exec.PumpStreamHandler;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
 public class Tests {
     @Test
@@ -47,6 +51,52 @@ public class Tests {
         System.out.println("退出代码: " + code);
         System.out.println(susStream.toString("GBK"));
         System.out.println(errStream.toString("GBK"));
+
+        // endregion
+
+        // region 判断命令是否存在
+
+        // 注意：不能直接执行 command 命令，因为它是 sh 内置的命令，所以需要使用 sh 执行此命令
+        commandLine = new CommandLine("sh");
+        commandLine.addArgument("-c");
+        // 使用 command -v 命令判断 libreoffice 命令是否存在
+        commandLine.addArgument("command -v libreoffice", false);
+        exec = DefaultExecutor.builder().get();
+        code = exec.execute(commandLine);
+        Assert.assertEquals(0, code);
+
+        commandLine = new CommandLine("sh");
+        commandLine.addArgument("-c");
+        // 使用 command -v 命令判断 libreoffice 命令是否存在
+        commandLine.addArgument("command -v libreofficex", false);
+        exec = DefaultExecutor.builder().get();
+        try {
+            exec.execute(commandLine);
+            Assert.fail();
+        } catch (ExecuteException ex) {
+            Assert.assertEquals(127, ex.getExitValue());
+        }
+
+        // endregion
+
+        // region 调用 libreoffice 命令转换 word 到 pdf
+
+        command = "libreoffice --headless --convert-to pdf --outdir /tmp/ /tmp/1.doc";
+        //接收正常结果流
+        susStream = new ByteArrayOutputStream();
+        //接收异常结果流
+        errStream = new ByteArrayOutputStream();
+        commandLine = CommandLine.parse(command);
+        exec = DefaultExecutor.builder().get();
+        streamHandler = new PumpStreamHandler(susStream, errStream);
+        exec.setStreamHandler(streamHandler);
+        code = exec.execute(commandLine);
+        Assert.assertEquals(0, code);
+        String successStr = susStream.toString(StandardCharsets.UTF_8.name());
+        String errorStr = errStream.toString(StandardCharsets.UTF_8.name());
+        Assert.assertTrue(successStr.startsWith("convert /tmp/1.doc -> /tmp/1.pdf"));
+        // 没有报错则返回空字符串
+        Assert.assertEquals("", errorStr);
 
         // endregion
     }
