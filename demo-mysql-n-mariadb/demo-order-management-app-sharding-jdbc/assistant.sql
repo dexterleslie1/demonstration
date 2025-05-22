f282d0b9902d7159ad8ccf3d1cd3ebfa1432d265
dexterleslie@gmail.com <dexterleslie@gmail.com>
2025-05-22 10:28:07 +0800

    存储过程文档添加declare和set变量定义区别说明

--- demo-mysql-n-mariadb/demo-order-management-app-sharding-jdbc/assistant.sql
+++ demo-mysql-n-mariadb/demo-order-management-app-sharding-jdbc/assistant.sql
@@ -2,17 +2,17 @@ delimiter |
 
 begin not atomic
 	declare var_counter int default 1;
-    set @total = 32;
+    declare var_total int default 32;
     set @sql = '';
     /*set @sql = 'select sum(count) from ( ';*/
-    while var_counter<=@total do
+    while var_counter<=var_total do
 		-- 使用 CONCAT 拼接表名
 		SET @table_name = CONCAT('t_order', var_counter);
 
 		-- 动态生成 SQL 语句
 		SET @sql = CONCAT(@sql,'select ', var_counter, ',count(id) as count from ', @table_name);
 
-		if var_counter<@total then
+		if var_counter<var_total then
 			set @sql = concat(@sql, ' union ');
         end if;
 
