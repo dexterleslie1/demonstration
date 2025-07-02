054ba8c7b02749366ae3d0a861f50721ab7cbfec
dexterleslie@gmail.com <dexterleslie@gmail.com>
2025-07-02 10:05:11 +0800

    可忽略提交

---
+++ demo-场景案例/demo-flash-sale/service/src/main/java/com/future/demo/config/ConfigSpring.java
@@ -0,0 +1,11 @@
+package com.future.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigSpring {
+    /*@Bean
+    public XRequestIdGenericFilterBean xRequestIdGenericFilterBean() {
+        return new XRequestIdGenericFilterBean();
+    }*/
+}
