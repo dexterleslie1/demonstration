54cbc61beb0e56df2174d7082d5317148b7316d9
dexterleslie@gmail.com <dexterleslie@gmail.com>
2025-09-05 08:29:26 +0800

    iOS新增UITableViewController demo

---
+++ demo-macos/demo-uitableviewcontroller/demo-uitableviewcontroller/TestUITableTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestUITableTableViewController.h
+//  demo-uitableviewcontroller
+//
+//  Created by dexterleslie on 2025/9/4.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestUITableTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
