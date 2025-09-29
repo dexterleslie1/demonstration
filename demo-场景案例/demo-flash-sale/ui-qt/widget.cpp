4de0e0eb404cfd88f6ae938333df52b7d295709c
dexterleslie <dexterleslie@gmail.com>
2025-09-29 16:42:14 +0800

    场景案例秒杀Qt UI实现用户ID、商家ID信息存储并跳转到主界面功能

---
+++ demo-场景案例/demo-flash-sale/ui-qt/widget.cpp
@@ -0,0 +1,41 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QSettings>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    // 创建主窗口
+    this->mainWidget = new MainWidget();
+
+    // 读取上次保存的 userId 和 merchantId
+    QString userId = this->qSettings.value("userId", "").toString();
+    QString merchantId = this->qSettings.value("merchantId", "").toString();
+    ui->iDLineEdit->setText(userId);
+    ui->iDLineEdit_2->setText(merchantId);
+
+    // 点击确定按钮
+    connect(ui->pushButton, &QPushButton::clicked, this, [this](){
+        // 获取 userId 和 merchantId
+        QString userId = ui->iDLineEdit->text();
+        QString merchantId = ui->iDLineEdit_2->text();
+
+        qSettings.setValue("userId", userId);
+        qSettings.setValue("merchantId", merchantId);
+        qSettings.sync();
+
+        // 跳转到主窗口
+        this->hide();
+        this->mainWidget->show();
+    });
+}
+
+Widget::~Widget()
+{
+    delete ui;
+    delete this->mainWidget;
+}
+
