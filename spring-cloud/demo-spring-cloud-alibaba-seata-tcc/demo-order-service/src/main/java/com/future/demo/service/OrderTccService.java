7409ba543900f16fa939697fb133913b06ffa711
dexterleslie@gmail.com <dexterleslie@gmail.com>
2025-10-12 00:56:38 +0800

    Seata TCC示例完全使用TCC实现

---
+++ spring-cloud/demo-spring-cloud-alibaba-seata-tcc/demo-order-service/src/main/java/com/future/demo/service/OrderTccService.java
@@ -0,0 +1,36 @@
+package com.future.demo.service;
+
+import com.future.common.exception.BusinessException;
+import com.future.demo.entity.Order;
+import io.seata.rm.tcc.api.BusinessActionContext;
+import io.seata.rm.tcc.api.BusinessActionContextParameter;
+import io.seata.rm.tcc.api.LocalTCC;
+import io.seata.rm.tcc.api.TwoPhaseBusinessAction;
+
+@LocalTCC
+public interface OrderTccService {
+    /**
+     * @param order
+     * @param throwExceptionWhenDeductBalance 扣减余额时是否抛出异常以模拟余额扣减失败
+     * @throws BusinessException
+     */
+    @TwoPhaseBusinessAction(name = "createOrder", commitMethod = "confirm", rollbackMethod = "cancel")
+    Long createOrder(@BusinessActionContextParameter(paramName = "order") Order order,
+                     boolean throwExceptionWhenDeductBalance) throws BusinessException;
+
+    /**
+     * 提交
+     *
+     * @param context
+     * @return
+     */
+    boolean confirm(BusinessActionContext context);
+
+    /**
+     * 回滚
+     *
+     * @param context
+     * @return
+     */
+    boolean cancel(BusinessActionContext context);
+}
