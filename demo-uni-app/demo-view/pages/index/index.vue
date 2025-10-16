b93cedef59d5fb9f739c1ebd803a9ee645e05c4d
dexterleslie <dexterleslie@gmail.com>
2025-10-16 11:24:03 +0800

    新增uni-app view示例

---
+++ demo-uni-app/demo-view/pages/index/index.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="container">
+		<view>A</view>
+		<view>B</view>
+		<view>C</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+	}
+	
+	.container view {
+		width: 100px;
+		line-height: 100px;
+		text-align: center;
+	}
+	
+	.container view:nth-child(1) {
+		background-color: red;
+	}
+	
+	.container view:nth-child(2){
+		background-color: green;
+	}
+	
+	.container view:nth-child(3) {
+		background-color: greenyellow;
+	}
+</style>
