c45f1d70f9ae9438dd285db612fc485e504b4a18
dexterleslie <dexterleslie@gmail.com>
2025-12-26 17:37:49 +0800

    可忽略提交

--- openresty/security-and-performance/config-limit-conn/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# docker 镜像版本
-version=1.0.0
-# docker 镜像前缀
-dockerRegistryPrefix=docker.118899.net:10001/yyd-public
-
-docker build --tag $dockerRegistryPrefix/demo-openresty-limit-conn:$version .
