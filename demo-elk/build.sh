26edfc1bf232fbe2bd86b267fa6c3b8cd4155109
dexterleslie@gmail.com <dexterleslie@gmail.com>
2026-02-27 10:49:22 +0800

    ELK示例添加基于ES8和ES9

--- demo-elk/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 编译elasticsearch
-docker build --tag registry.cn-hangzhou.aliyuncs.com/future-public/demo-elk-elasticsearch --file Dockerfile-elasticsearch .
-docker build --tag registry.cn-hangzhou.aliyuncs.com/future-public/demo-elk-logstash --file Dockerfile-logstash .
-docker build --tag registry.cn-hangzhou.aliyuncs.com/future-public/demo-elk-curator --file Dockerfile-curator .
--- demo-elk/push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker push registry.cn-hangzhou.aliyuncs.com/future-public/demo-elk-elasticsearch
-docker push registry.cn-hangzhou.aliyuncs.com/future-public/demo-elk-logstash
-docker push registry.cn-hangzhou.aliyuncs.com/future-public/demo-elk-curator
-
